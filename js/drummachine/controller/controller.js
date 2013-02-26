13cbdbc537b3bfaee0deee8a3d7e718b894f3aca
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2013-02-26 23:04:15 +0000

    controller renamed

---
+++ js/drummachine/controller/controller.js
@@ -0,0 +1,35 @@
+define([], function() {
+        
+        var Controller = function( newUI )
+        { 
+            var setUpEvents,
+                handleSegment,
+                ui;
+
+        var setUI = function( newUI )
+        {
+            ui = newUI;
+        };
+
+        var setUpEvents = function()
+        {
+            ui.getCtx().on( 'click', '.beat', handleSegment );
+        };
+
+        var handleSegment = function( e )
+        {
+            var index = $(this).index();
+            $(window).trigger('ui:button:segment', { index: index } );
+        };
+
+        var init = function( newUI )
+        {
+                setUI( newUI );
+                setUpEvents();
+        };
+
+        init( newUI );
+        };
+
+        return Controller;
+});
