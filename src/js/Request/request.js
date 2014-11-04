1bb2d09a9d90e73e5e2d27703f0171fda92a9014
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2014-11-04 19:27:58 +0000

    added some request helpers

---
+++ src/js/Request/request.js
@@ -0,0 +1,28 @@
+var Promise = require('es6-promise').Promise;
+
+var request = function(url, requestType, responseType) {
+	// Return a new promise.
+	return new Promise(function(resolve, reject) {
+		var request = new XMLHttpRequest();
+		request.open(requestType, url, true);
+		request.responseType = responseType;
+
+		request.onload = function() {
+			resolve(request.response);
+		};
+		request.onerror = function() {
+			reject(Error(request.statusText));
+		};
+
+		request.send();
+	});
+};
+
+var getBuffer = function(url) {
+	return get('GET', url, 'arraybuffer');
+};
+
+module.exports = {
+	request: request,
+	getBuffer: getBuffer
+};
