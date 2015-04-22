f2454beed291669474ae8db03abc424c0b977d6f
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-04-22 22:09:34 +0100

    add clap sounds

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@ var onContext = require('./AudioAPI/context').context,
 	getBuffer = require('./Request/request').getBuffer;
 
 var onKick = getBuffer('samples/808/01_KCK1.WAV');
-var onClap = getBuffer('samples/808/15_CLP1.WAV');
+var onClap = getBuffer('samples/808/15_CLP2.WAV');
 var kickBuffer = null;
 var clapBuffer = null;
 var kicks = [1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0];
-var clap = [0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1];
+var claps = [0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,1];
 var bpm = 120;
 var secondsInMinute = 60;
 var beatsPerBar = 4;
@@ -22,15 +22,23 @@ var playSound = function(buffer, context, time) {
 	source.start(time || context.currentTime);                           // play the source now
 };
 
-Promise.all([onContext, onKick]).then(function(promises){
+Promise.all([onContext, onKick, onClap]).then(function(promises){
 	var context = promises[0];
 	var kick = promises[1];
-	contextHelper.decodeAudioData(kick, context)
-	.then(function(buffer) {
-		kickBuffer = buffer;
+	var clap = promises[2];
+	var kickDecode = contextHelper.decodeAudioData(kick, context);
+	var clapDecode = contextHelper.decodeAudioData(clap, context);
+	Promise.all([kickDecode, clapDecode])
+	.then(function(promises){
+		var kickBuffer = promises[0];
+		var clapBuffer = promises[1];
 		kicks.forEach(function(kick, i){
 			if(!kick) return;
 			playSound(kickBuffer, context, (i + 1) * segmentTime);
+		});
+		claps.forEach(function(clap, i){
+			if(!clap) return;
+			playSound(clapBuffer, context, (i + 1) * segmentTime);
 		});
     });
 });
