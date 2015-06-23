c21d4b00a9037af155cebb5c48456a28b21ddcd4
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-06-23 13:50:51 +0100

    move towards classes or singletons for pattern information

--- src/js/AudioAPI/Tempo.js
+++ src/js/AudioAPI/Tempo.js
@@ -1,13 +1,13 @@
+var secondsInMinute = 60;
+
 class Tempo {
-	constructor() {
-		this.beatsPerMinute = 120;
+	constructor(data) {
+		this.beatsPerMinute = data.beatsPerMinute || 120;
 		this.beatsPerBar = 4;
 		this.segmentsPerBeat = 4;
-
-		this.secondsInMinute = 60;
 	}
 	getBeatsPerSecond() {
-		return this.secondsInMinute / this.beatsPerMinute;
+		return secondsInMinute / this.beatsPerMinute;
 	}
 	getSegmentTimeInSeconds(){
 		return this.getBeatsPerSecond() / this.segmentsPerBeat;
--- src/js/main.js
+++ src/js/main.js
@@ -2,31 +2,46 @@ import { WebAudioContext } from './AudioAPI/WebAudioContext';
 import { Tempo } from './AudioAPI/Tempo';
 import { getBuffer } from './Request/request';
 
-var tempo = new Tempo();
+var tempoData = {
+	beatsPerMinute: 120,
+	beatsPerBar: 4,
+	segmentsPerBeat: 4
+};
+var tempo = new Tempo(tempoData);
 var context = new WebAudioContext();
-var sounds = ['samples/808/01_KCK1.WAV', 'samples/808/15_CLP2.WAV'];
-var kicks = [1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0];
-var claps = [0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,1];
+var soundsData = [{
+		name: 'kick',
+		path: 'samples/808/01_KCK1.WAV',
+		patterns: [
+			[1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0]
+		]
+	},{
+		name: 'clap',
+		path: 'samples/808/15_CLP2.WAV',
+		patterns: [
+			[0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,1]
+		]
+	}];
 
-Promise.all(sounds.map(getBuffer))
+Promise.all(soundsData.map(function(item){
+	return item.path;
+})
+.map(getBuffer))
 .then(function(promises){
 	return Promise.all(promises.map(function(sound){
 		return context.decodeAudioData(sound);
 	}));
 })
 .then(function(promises){
-	var kickBuffer = promises[0];
-	var clapBuffer = promises[1];
-	kicks.forEach(function(kick, i){
-		if(!kick) return;
-		context.playSound(kickBuffer, (i + 5) * tempo.getSegmentTimeInSeconds());
-	});
-	claps.forEach(function(clap, i){
-		if(!clap) return;
-		context.playSound(clapBuffer, (i + 5) * tempo.getSegmentTimeInSeconds());
+	promises.map(function(buffer, index){
+		var pattern = soundsData[index].patterns[0];
+		pattern.forEach(function(segment, i){
+			if(!segment) return;
+			context.playSound(buffer, (i + 5) * tempo.getSegmentTimeInSeconds());
+		});
 	});
 })
-.catch(console.log.bind(console));;
+.catch(console.log.bind(console));
 
 var React = require('react');
 
