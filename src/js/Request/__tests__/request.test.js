16872ea89e376aa72f78927270b46fbefa5319d8
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-06-29 21:34:15 +0100

    request tests

--- src/js/Request/__tests__/request.test.js
+++ src/js/Request/__tests__/request.test.js
@@ -1,19 +1,14 @@
-jest.dontMock('../request');
-var request = require('../request').request;
+jest.dontMock("../request");
+var request = require("../request").request,
+	Promise = require("es6-promise").Promise;
 
-describe('request', function() {
+describe("request", function() {
 	var requestPromise;
 	beforeEach(function(){
 		window.XMLHttpRequest = jest.genMockFunction();
-		requestPromise = request('dummy.html', "GET", "arraybuffer");
 	});
 	it("should create and return a promise", function() {
-		expect(window.Promise.mock.instances.length).toBe(1);
-	});
-	pit("should call an XMLHttpRequest with the correct parameters", function() {
-		return requestPromise.then(function(stuff) {
-			expect(window.XMLHttpRequest.mock.instances.length).toBe(1);
-			expect(window.XMLHttpRequest.mock.instances[0].onload.calls[0]).toBe(1);
-		});
+		requestPromise = request("dummy.html", "GET", "arraybuffer");
+		expect(requestPromise instanceof Promise).toBe(true);
 	});
 });
