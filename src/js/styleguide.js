bf8459c40e8fb94b7bb341fd6ecc465e25662406
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-06-29 17:30:07 +0100

    signature added

--- src/js/styleguide.js
+++ src/js/styleguide.js
@@ -4,6 +4,7 @@ import { Display } from "./components/display/display.react";
 React.render(
 	(<div className="drum-machine">
 		<Display name="Tempo" value="120" />
+		<Display name="Signature" value="4/4" />
 	</div>),
 	document.getElementById("drum-machine")
 );
