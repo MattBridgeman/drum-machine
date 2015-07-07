cb99c75551ea6b25fdc80fe24595d9018adf1eb6
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-07-07 22:06:14 +0100

    kick pattern display work

---
+++ src/js/components/beat/beat.react.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+
+class Beat extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: props.name,
+			value: props.value,
+			beats: props.beats
+		};
+	}
+
+	render() {
+		var expandedClass = this.state.expanded ? "open" : "closed";
+		var expandedName = this.state.expanded ? "close" : "expand";
+		var displayClass = "display beat unit red";
+		var expandableButtonClass = "button expandable " + expandedClass;
+		var beats = this.getBeats();
+		return (
+			<div className={displayClass}>
+				<div className="inner">
+					<p className="name">{this.state.name}</p>
+					<p className="value">{this.state.value}</p>
+				</div>
+				<div className="inner beat-map">
+				hello
+					{beats}
+				</div>
+				<button className={expandableButtonClass}>{expandedName} display</button>
+			</div>
+		);
+	}
+
+	getBeats(){
+		return this.state.beats.map((beat, index) => 
+			(
+				<button class="button">1/16</button>
+			)
+		)
+	}
+
+};
+
+Beat.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	value: React.PropTypes.any.isRequired,
+	beats: React.PropTypes.array.isRequired
+};
+
+Beat.defaultProps = { colour: "green" };
+
+export { Beat };
--- src/js/styleguide.js
+++ src/js/styleguide.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Display } from "./components/display/display.react";
+import { Beat } from "./components/beat/beat.react";
 import { PlayHeading } from "./components/play-heading/play.heading.react";
 
 React.render(
@@ -8,6 +9,7 @@ React.render(
 			<PlayHeading isPlaying={true} value="00:01" />
 			<Display name="Tempo" value="120" />
 			<Display name="Signature" value="4/4" />
+			<Beat name="Kick" value="1/16" beats={[1,0,0,0]} />
 		</div>
 	),
 	document.getElementById("drum-machine")
