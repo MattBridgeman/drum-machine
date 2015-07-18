a5b5fe31001a7648ea2ebc8b4b2c9ec8155e40e7
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-07-18 21:57:41 +0100

    start working into an app architecture

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,7 @@
-import { WebAudioContext } from "./audio-api/context";
-import { Tempo } from "./audio-api/tempo";
-import { arrayBuffer } from "./request/arraybuffer";
 import * as React from "react";
-import { HelloWorld } from "./components/helloworld.react";
+import { DrumMachine } from "./components/drum-machine/drum.machine.react";
 
-var data = {
-	tempo: {
-		beatsPerMinute: 120,
-		beatsPerBar: 4,
-		segmentsPerBeat: 4
-	},
-	selectedSoundIndex: 0,
-	sounds: [{
-		name: "kick",
-		path: "samples/808/01_KCK1.WAV"
-	}, {
-		name: "clap",
-		path: "samples/808/15_CLP2.WAV"
-	}],
-	patterns: [{
-		name: "kick",
-		patterns: [
-			[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0]
-		]
-	}, {
-		name: "kick",
-		patterns: [
-			[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1]
-		]
-	}]
-};
-var tempo = new Tempo(data.tempo);
-var context = new WebAudioContext();
-
-Promise.all(data.sounds.map(function(item){
-	return item.path;
-})
-.map(arrayBuffer))
-.then(function(promises){
-	return context.decodeAudioDataArray(promises);
-})
-.then(function(promises){
-	promises.map(function(buffer, index){
-		var pattern = data.patterns[index].patterns[0];
-		pattern.forEach(function(segment, i){
-			if(!segment) {
-				return;
-			}
-			context.playSound(buffer, (i + 5) * tempo.getSegmentTimeInSeconds());
-		});
-	});
-})
-.catch(console.log.bind(console));
+React.render(
+  <DrumMachine />,
+  document.getElementById('drum-machine')
+);
