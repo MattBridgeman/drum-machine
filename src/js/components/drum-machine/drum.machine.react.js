5e1230f435096ee44f31eb2214009648fb215217
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-07-18 23:28:32 +0100

    extract data into its own store

--- src/js/components/drum-machine/drum.machine.react.js
+++ src/js/components/drum-machine/drum.machine.react.js
@@ -3,33 +3,8 @@ import { Tempo } from "../../audio-api/tempo";
 import { arrayBuffer } from "../../request/arraybuffer";
 import * as React from "react";
 import { PlayHeading } from "../play-heading/play.heading.react";
+import { data } from './stores/data.store';
 
-var data = {
-	tempo: {
-		beatsPerMinute: 120,
-		beatsPerBar: 4,
-		segmentsPerBeat: 4
-	},
-	selectedSoundIndex: 0,
-	sounds: [{
-		name: "kick",
-		path: "samples/808/01_KCK1.WAV"
-	}, {
-		name: "clap",
-		path: "samples/808/15_CLP2.WAV"
-	}],
-	patterns: [{
-		name: "kick",
-		patterns: [
-			[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0]
-		]
-	}, {
-		name: "kick",
-		patterns: [
-			[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1]
-		]
-	}]
-};
 var tempo = new Tempo(data.tempo);
 var context = new WebAudioContext();
 
---
+++ src/js/components/drum-machine/stores/data.store.js
@@ -0,0 +1,28 @@
+var data = {
+	tempo: {
+		beatsPerMinute: 120,
+		beatsPerBar: 4,
+		segmentsPerBeat: 4
+	},
+	selectedSoundIndex: 0,
+	sounds: [{
+		name: "kick",
+		path: "samples/808/01_KCK1.WAV"
+	}, {
+		name: "clap",
+		path: "samples/808/15_CLP2.WAV"
+	}],
+	patterns: [{
+		name: "kick",
+		patterns: [
+			[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0]
+		]
+	}, {
+		name: "kick",
+		patterns: [
+			[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1]
+		]
+	}]
+};
+
+export { data };
