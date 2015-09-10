d8f6f497274465dfc940f23419f47bace2b91458
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-09-10 13:59:51 +0100

    refactor store

--- src/js/components/drum-machine/reducers/drum.machine.reducers.js
+++ src/js/components/drum-machine/reducers/drum.machine.reducers.js
@@ -6,25 +6,30 @@ const initialState = {
 		beatsPerBar: 4,
 		segmentsPerBeat: 4
 	},
-	selectedSoundIndex: 0,
-	sounds: [{
-		name: "kick",
-		path: "samples/808/01_KCK1.WAV"
-	}, {
-		name: "clap",
-		path: "samples/808/15_CLP2.WAV"
-	}],
-	patterns: [{
-		name: "kick",
-		patterns: [
-			[0, 4, 8, 12, 14]
-		]
-	}, {
-		name: "kick",
-		patterns: [
-			[4, 12, 13, 15]
-		]
-	}],
+	sounds: {
+		0: {
+			name: "kick",
+			path: "samples/808/01_KCK1.WAV"
+		},
+		1: {
+			name: "clap",
+			path: "samples/808/15_CLP2.WAV"
+		}
+	},
+	channels: {
+		0: {
+			sound: 0,
+			patterns: [0]
+		},
+		1: {
+			sound: 1,
+			patterns: [1]
+		}
+	},
+	patterns: {
+		0: [0, 4, 8, 12, 14],
+		1: [4, 12, 13, 15]
+	},
 	isPlaying: false
 };
 
