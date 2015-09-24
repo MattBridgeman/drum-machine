e6c4afc14f84f1c155bf2744e1b929c5b6bbcdc4
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-09-24 13:21:40 +0100

    tempo increment BPM passing test

--- src/js/components/drum-machine/reducers/__tests__/tempo.test.js
+++ src/js/components/drum-machine/reducers/__tests__/tempo.test.js
@@ -1,8 +1,24 @@
 import {expect} from "chai";
 import tempo from "../tempo.reducer";
+import { INCREMENT_BPM, DECREMENT_BPM } from '../../constants/drum.machine.constants';
 
 describe("Tempo reducer", function() {
 	it("Expect tempo to increase to 121", function() {
-		expect(2).to.equal(1);
+		const initialState = {
+			tempo: {
+				beatsPerMinute: 120,
+				beatsPerBar: 4,
+				segmentsPerBeat: 4
+			}
+		};
+		
+		const action = {
+			type: INCREMENT_BPM
+		};
+		
+		const nextState = tempo(initialState, action);
+		
+		expect(initialState.tempo.beatsPerMinute).to.equal(120);
+		expect(nextState.tempo.beatsPerMinute).to.equal(121);
 	});
 });
--- src/js/components/drum-machine/reducers/tempo.reducer.js
+++ src/js/components/drum-machine/reducers/tempo.reducer.js
@@ -10,31 +10,13 @@ const initialState = {
 
 export default function tempo(state = initialState, action) {
 	switch (action.type) {
-		case PLAY:
-		//TODO: use immutable library
-		return Object.assign({}, state, { isPlaying: true });
-
-        case PAUSE:
-		//TODO: use immutable library
-		return Object.assign({}, state, { isPlaying: false });
-
-        case TOGGLE_PLAY_PAUSE:
-		//TODO: use immutable library
-		return Object.assign({}, state, { isPlaying: !state.isPlaying });
-
-        case TOGGLE_PLAY_PAUSE:
-		//TODO: use immutable library
-		return Object.assign({}, state, { isPlaying: !state.isPlaying });
-		
-		TOGGLE_BEAT_STATE:
-		//TODO: use immutable library
-		return Object.assign({}, state, {
-			beats: {
-				[action.id]: state.beats[action.id] ? 0 : 1
-			}
-		});
-
+		case INCREMENT_BPM:
+			return Object.assign({}, state, {
+				tempo: {
+					beatsPerMinute: state.tempo.beatsPerMinute + 1
+				}
+			});
 		default:
-		return state;
+			return state;
 	}
 }
