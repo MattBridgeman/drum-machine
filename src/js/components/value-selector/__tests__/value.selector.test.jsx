1df16a53bc57dc4c6ec08af544d747ce0c4a488e
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-10-10 15:16:42 +0100

    passing test for value select 'value'

--- src/js/components/value-selector/__tests__/value.selector.test.jsx
+++ src/js/components/value-selector/__tests__/value.selector.test.jsx
@@ -7,32 +7,42 @@ const { renderIntoDocument, Simulate } = React.addons.TestUtils;
 describe("Value Selector", () => {
 
   it("renders a value selector", () => {
-    const component = renderIntoDocument(
+    const $component = renderIntoDocument(
       <ValueSelector />
     );
-    expect(typeof component).to.equal("object");
+    expect($component).to.be.a("object");
   });
 
   it("triggers 'on increment' callback when button is pressed", () => {
 		var incremented = false;
-    const component = renderIntoDocument(
+    const $component = renderIntoDocument(
       <ValueSelector onIncrement={() => incremented = true} />
     );
-    var incrementButton = component.refs.incrementButton;
+    var $incrementButton = $component.refs.incrementButton;
     expect(incremented).to.equal(false);
-    Simulate.click(incrementButton);
+    Simulate.click($incrementButton);
     expect(incremented).to.equal(true);
   });
 
   it("triggers 'on decrement' callback when button is pressed", () => {
 		var decremented = false;
-    const component = renderIntoDocument(
+    const $component = renderIntoDocument(
       <ValueSelector onDecrement={() => decremented = true} />
     );
-    var decrementButton = component.refs.decrementButton;
+    var $decrementButton = $component.refs.decrementButton;
     expect(decremented).to.equal(false);
-    Simulate.click(decrementButton);
+    Simulate.click($decrementButton);
     expect(decremented).to.equal(true);
   });
+
+  it("displays the correct value recieved via props", () => {
+		var value = 120;
+    const $component = renderIntoDocument(
+      <ValueSelector value={value} />
+    );
+    var $value = $component.refs.value;
+    expect($value.getDOMNode().textContent).to.equal(value.toString());
+  });
+
 
 });
--- src/js/components/value-selector/value.selector.react.jsx
+++ src/js/components/value-selector/value.selector.react.jsx
@@ -5,12 +5,12 @@ class ValueSelector extends React.Component {
 	constructor(props) {
 		super(props);
 	}
-	
+
 	render() {
 		var { value, onIncrement, onDecrement } = this.props;
 		return (
 			<div className="source-selector channel-item">
-				<h3 className="selected">{value}</h3><button ref="incrementButton" onClick={onIncrement} className="button">+</button><button ref="decrementButton" onClick={onDecrement} className="button">-</button>
+				<h3 ref="value" className="selected">{value}</h3><button ref="incrementButton" onClick={onIncrement} className="button">+</button><button ref="decrementButton" onClick={onDecrement} className="button">-</button>
 			</div>
 		);
 	}
