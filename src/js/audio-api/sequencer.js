ef9de547611e0ec8f0bf745acbeda0292e29b0d2
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-10-15 13:48:25 +0100

    class based sequencer

--- src/js/audio-api/sequencer.js
+++ src/js/audio-api/sequencer.js
@@ -2,6 +2,31 @@ export function sequencer(store){
 	// let unsubscribe = store.subscribe(() =>
 	// 	console.log(store.getState())
 	// );
+	
+	function update(){
+		var state = store.getState();
+		if(!state.isPlaying) return;
+		setTimeout(update, 30);
+	}
+}
+
+class Sequencer {
+	constructor(store) {
+		this.store = store;
+		this.setUpEvents();
+	}
+	setUpEvents(){
+		this.store.subscribe(() => this.tick());
+	}
+	tick() {
+		let state = this.store.getState();
+		setTimeout(() => this.tick(), 30);
+		if(!state.isPlaying) return;
+		this.update();
+	}
+	update(){
+		
+	}
 }
 
 // function update(state){
