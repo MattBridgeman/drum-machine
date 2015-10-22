6046afb73c11344e3b38d5122db807826700c809
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-10-22 13:38:07 +0100

    get sounds from all available

--- src/js/middleware/buffer.js
+++ src/js/middleware/buffer.js
@@ -1,37 +1,20 @@
-import { PLAY, TOGGLE_PLAY_PAUSE, NEW_SEGMENT_INDEX } from "../constants/play.state.constants";
-import { getSegmentsToBuffer } from "../library/audio-api/tempo";
+import { TOGGLE_PLAY_PAUSE, NEW_SEGMENT_INDEX } from "../constants/play.state.constants";
+import { loadSounds } from "../library/audio-api/buffer";
+import { WebAudioContext } from "../library/audio-api/context";
 
-export let createBufferStream = (getSegmentTime) => Rx.Observable.create(function (observer) {
-	let startTime = new Date().getTime(),
-		frameId,
-		loop,
-		tick;
-
-	loop = function() {
-		if(!frameId){
-			observer.onNext(1);
-		}
-		let now = new Date().getTime();
-		let deltaTime = now - startTime;
+export const createBuffer = store => {
+	let context = new WebAudioContext();
+	let buffers = loadSounds(store);
+	let sounds = Promise.all(buffers)
+		.then((promises) => context.decodeAudioDataArray(promises));
 		
+	sounds.then(arrayBuffers => console.log(arrayBuffers));
+	
+	return next => action => {
+		if (action.type !== TOGGLE_PLAY_PAUSE || action.type !== NEW_SEGMENT_INDEX) {
+			return next(action);
+		}
 		let state = store.getState();
-		let { beatsPerMinute, segmentsPerBeat } = state.tempo;
-		let segmentTime = getSegmentTimeInMilliseconds(beatsPerMinute, segmentsPerBeat);
-		
-		if(deltaTime >= segmentTime) {
-			observer.onNext(1);
-			startTime = now;
-		}
-		tick();
+		let { channels } = state;
 	};
-
-	tick = function() {
-		frameId = requestAnimationFrame(loop);
-	};
-
-	loop();
-
-    return function () {
-        cancelAnimationFrame(frameId);
-    };
-});
+};
--- src/js/store/drum.machine.store.js
+++ src/js/store/drum.machine.store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/drum.machine.root.reducer";
 import { sequencer } from "../middleware/scheduler";
+import { createBuffer } from "../middleware/buffer";
 
 export default function configureStore(initialState) {
-	let createStoreWithMiddleware = applyMiddleware(sequencer)(createStore);
+	let createStoreWithMiddleware = applyMiddleware(sequencer, createBuffer)(createStore);
 	const store = createStoreWithMiddleware(rootReducer, initialState);
 	return store;
 }
