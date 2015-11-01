19feae218265ca0cdbe43e22424390198ab1d5cc
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-11-01 16:09:02 +0000

    play state reducer complexity

--- src/js/reducers/play.state.reducer.js
+++ src/js/reducers/play.state.reducer.js
@@ -1,4 +1,5 @@
-import { TOGGLE_PLAY_PAUSE, NEW_SEGMENT_INDEX, INCREMENT_SEGMENT_INDEX } from "../constants/play.state.constants";
+import { PLAY, PAUSE, TOGGLE_PLAY_PAUSE, NEW_SEGMENT_INDEX, INCREMENT_SEGMENT_INDEX } from "../constants/play.state.constants";
+import { play, pause, newSegmentIndex } from "../actions/play.state.actions";
 
 const initialState = {
 	currentSegmentIndex: 0,
@@ -9,16 +10,25 @@ const initialState = {
 
 export default function playState(state = initialState, action) {
 	switch (action.type) {
-		case TOGGLE_PLAY_PAUSE:
-			return Object.assign({}, state, {
-				isPlaying: !state.isPlaying,
-				currentSegmentIndex: state.isPlaying ? state.currentSegmentIndex : 0
+		case PLAY:
+			const newState = Object.assign({}, state, {
+				isPlaying: true
 			});
+			return playState(newState, newSegmentIndex(0));
+		case PAUSE:
+			return Object.assign({}, state, { isPlaying: false });
+		case TOGGLE_PLAY_PAUSE:
+			if(state.isPlaying){
+				return playState(state, pause());
+			} else {
+				return playState(state, play());
+			}
+			break;
 		case NEW_SEGMENT_INDEX:
 			return Object.assign({}, state, { currentSegmentIndex: action.value });
 		case INCREMENT_SEGMENT_INDEX:
-			let currentSegmentIndex = state.looping && state.currentSegmentIndex >= 15 ? 0 : state.currentSegmentIndex + 1;
-			return Object.assign({}, state, { currentSegmentIndex });
+			let newIndex = state.looping && state.currentSegmentIndex >= 15 ? 0 : state.currentSegmentIndex + 1;
+			return playState(state, newSegmentIndex(newIndex));
 		default:
 			return state;
 	}
