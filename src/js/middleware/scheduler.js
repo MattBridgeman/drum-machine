e5d365f697d5670b0676dfd4fe96d7b5ebb0e8e8
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-11-01 17:59:25 +0000

    segmentStream added inside of a closure

--- src/js/middleware/scheduler.js
+++ src/js/middleware/scheduler.js
@@ -16,18 +16,19 @@ let createSegmentStream = (store) => {
 	);
 };
 
-let segmentStream;
+export const sequencer = () => {
+	let segmentStream;
+	return store => next => action => {
+		if (action.type !== PLAY && action.type !== PAUSE && action.type !== TOGGLE_PLAY_PAUSE ) {
+			return next(action);
+		}
+		let state = store.getState();
+		if((action.type === TOGGLE_PLAY_PAUSE && state.playState.isPlaying === false)) {
+			segmentStream = createSegmentStream(store).subscribe(() => next(incrementSegmentIndex()));
+		} else if ((action.type === TOGGLE_PLAY_PAUSE && state.playState.isPlaying === true)) {
+			segmentStream.dispose();
+		}
 
-export const sequencer = store => next => action => {
-	if (action.type !== PLAY && action.type !== PAUSE && action.type !== TOGGLE_PLAY_PAUSE ) {
 		return next(action);
-	}
-	let state = store.getState();
-	if((action.type === TOGGLE_PLAY_PAUSE && state.playState.isPlaying === false)) {
-		segmentStream = createSegmentStream(store).subscribe(() => next(incrementSegmentIndex()));
-	} else if ((action.type === TOGGLE_PLAY_PAUSE && state.playState.isPlaying === true)) {
-		segmentStream.dispose();
-	}
-
-	return next(action);
+	};
 };
