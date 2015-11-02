f2d03dced8868ea78b24ab9733be0d2f78fbafb4
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2015-11-02 14:57:19 +0000

    done callback not needed

--- src/js/library/audio-api/__tests__/interval.test.js
+++ src/js/library/audio-api/__tests__/interval.test.js
@@ -4,7 +4,7 @@ import * as Rx from "Rx";
 
 describe("Interval", () => {
 	
-	it("calls adds to the segment stream if current time is greater than interval time", (done) => {
+	it("calls adds to the segment stream if current time is greater than interval time", () => {
 		let startTime = 0;
 		let currentTime = 126;
 		let intervalTime = 125;
@@ -19,7 +19,6 @@ describe("Interval", () => {
 		
 		segmentStream.subscribe(value => {
 			expect(value).to.equal(1);
-			done();
 		});
 		
 		subject.onNext(1);
