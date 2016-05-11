77cf0cefdced00c0e3e8bb879047cc1e3ba97f71
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2016-05-11 14:18:30 +0100

    channel component now supports 'selected' state

---
+++ src/js/components/channel/__tests__/channel.react.test.jsx
@@ -0,0 +1,45 @@
+import TestUtils from "react-addons-test-utils";
+import React from "react";
+import { Channel } from "../channel.react.jsx";
+import { expect } from "chai";
+
+const { renderIntoDocument, Simulate } = TestUtils;
+
+describe("Channel", () => {
+
+	it("renders a Channel", () => {
+		let $component = renderIntoDocument(
+			<Channel />
+		);
+		expect($component).to.be.a("object");
+	});
+
+	it("displays the initial name", () => {
+		let expectedName = "Channel name";
+		let $component = renderIntoDocument(
+			<Channel name={expectedName} />
+		);
+		let { name } = $component.refs;
+		expect(name.textContent).to.equal(expectedName);
+	});
+
+	it("displays an unselected button, given selected=false prop", () => {
+		let selected = false;
+		let $component = renderIntoDocument(
+			<Channel selected={selected} />
+		);
+		let { toggleButton } = $component.refs;
+		expect(toggleButton.classList.contains("selected")).to.equal(false);
+		expect(toggleButton.getAttribute('aria-pressed')).to.equal('false');
+	});
+
+	it("displays an selected button, given selected=true prop", () => {
+		let selected = true;
+		let $component = renderIntoDocument(
+			<Channel selected={selected} />
+		);
+		let { toggleButton } = $component.refs;
+		expect(toggleButton.classList.contains("selected")).to.equal(true);
+		expect(toggleButton.getAttribute('aria-pressed')).to.equal('true');
+	});
+});
--- src/js/components/channel/channel.react.jsx
+++ src/js/components/channel/channel.react.jsx
@@ -10,12 +10,16 @@ class Channel extends React.Component {
 		var { name } = this.props;
 		var title = name ? (
 			<div className="channel-item">
-				<h3 className="item-title">{name}</h3>
+				<h3 ref="name" className="item-title">{name}</h3>
 			</div>
 		) : null;
+		var toggleClass = this.props.selected ? "selected" : "";
+		var ariaPressed = this.props.selected ? "true" : "false";
+		var toggleButton = <button ref="toggleButton" className={"button toggle-button " + toggleClass} aria-pressed={ariaPressed}>Select</button>;
 		return (
 			<div className="channel">
 				{title}
+				{toggleButton}
 				{this.props.children}
 			</div>
 		);
