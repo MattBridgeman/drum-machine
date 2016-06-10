fee0249a392bcfbeccc935dcb5d8c9145211ec55
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2016-06-10 18:23:20 +0100

    new set of samples

--- src/js/reducers/sounds.reducer.js
+++ src/js/reducers/sounds.reducer.js
@@ -3,39 +3,39 @@
 const initialState = {
 	0: {
 		name: "Bass Drum",
-		path: "samples/909/bd01.WAV"
+		path: "samples/909-2/bd01.WAV"
 	},
 	1: {
 		name: "Snare Drum",
-		path: "samples/909/sd01.WAV"
+		path: "samples/909-2/sd01.WAV"
 	},
 	2: {
 		name: "Low Tom",
-		path: "samples/909/lt01.WAV"
+		path: "samples/909-2/lt01.WAV"
 	},
 	3: {
 		name: "Mid Tom",
-		path: "samples/909/mt01.WAV"
+		path: "samples/909-2/mt01.WAV"
 	},
 	4: {
 		name: "Hi Tom",
-		path: "samples/909/ht01.WAV"
+		path: "samples/909-2/ht01.WAV"
 	},
 	5: {
 		name: "Rim Shot",
-		path: "samples/909/rs01.WAV"
+		path: "samples/909-2/rs01.WAV"
 	},
 	6: {
 		name: "Hand Clap",
-		path: "samples/909/cp01.WAV"
+		path: "samples/909-2/cp01.WAV"
 	},
 	7: {
 		name: "Hi Hat",
-		path: "samples/909/hh01.WAV"
+		path: "samples/909-2/hh01.WAV"
 	},
 	8: {
 		name: "Cymbal",
-		path: "samples/909/rd01.WAV"
+		path: "samples/909-2/rd01.WAV"
 	}
 };
 
