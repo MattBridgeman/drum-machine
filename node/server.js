34eb94706b688f7f38f18798e949dd99bafd74ac
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2016-06-22 12:53:16 +0100

    change server to reference '__dirname'

--- node/server.js
+++ node/server.js
@@ -1,25 +1,11 @@
 var connect 	= require('connect'),
 	fs			= require('fs'),
 	__args		= process.argv,
-	__dirname 	= [
-					'/Users/mbridgeman/Documents/labs/drum-machine/',
-					'/Users/matthewbridgeman/Documents/repo/labs/drum-machine/'	
-				],
-	port 		= 8081,
-	name		= '';
-
-(function(){
-	__dirname.forEach(function(dir){
-		try {
-			if(fs.lstatSync(dir).isDirectory()) name = dir;
-		} catch(e){
-			
-		}
-	});
-})();
+	__dir		= __dirname + '/../build/',
+	port 		= 8081;
 
 connect.createServer(
-	connect.static(name)
+	connect.static(__dir)
 ).listen(port);
 
 module.exports = connect;
