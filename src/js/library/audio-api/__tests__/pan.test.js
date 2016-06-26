93fdee34521cf7eb1ed266e5321237a10057ec8b
Matt Bridgeman <matt_bridgeman@hotmail.co.uk>
2016-06-26 16:02:09 +0100

    fix broken tests

--- src/js/library/audio-api/__tests__/pan.test.js
+++ src/js/library/audio-api/__tests__/pan.test.js
@@ -5,17 +5,17 @@ describe("Pan", () => {
 	it("goes to min pan given a percentage of '0'", () => {
 		const pan = panPercentageToValue(0);
 		
-		expect(pan).to.equal(MIN_PAN);
+		expect(pan).to.deep.equal([-1, 0, 0]);
 	});
 	it("goes to max pan given a percentage of '100'", () => {
 		const pan = panPercentageToValue(100);
 		
-		expect(pan).to.equal(MAX_PAN);
+		expect(pan).to.deep.equal([1, 0, 0]);
 	});
 	it("goes half way between min and max pan given a percentage of '50'", () => {
 		const pan = panPercentageToValue(50);
 		
-		expect(pan).to.equal(0);
+		expect(pan).to.deep.equal([0, 0, 1]);
 	});
 });
 		
