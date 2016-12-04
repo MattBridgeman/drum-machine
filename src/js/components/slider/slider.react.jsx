7d963325f46e3b822c63ef418f62fc2cbd8deeec
Matthew Bridgeman <mattbridgeman@YachtWorlds-MacBook-Pro.local>
2016-12-04 21:25:18 +0000

    Slider responds to current tempo

--- src/js/components/slider/slider.react.jsx
+++ src/js/components/slider/slider.react.jsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { rangeToArray } from "../../library/natives/array";
 
+const STEP_SIZE = 33.3;
+const STEP_OFFSET = -1;
+
 class Slider extends React.Component {
 
 	constructor(props) {
@@ -8,14 +11,21 @@ class Slider extends React.Component {
 	}
 	
 	render() {
-    let { min, max, step, onChange } = this.props;
-    debugger;
+    let { min, max, step, value, onChange } = this.props;
     let steps = rangeToArray(min, max, step);
+    let minStep = 1;
+    let maxStep = steps.length;
+    let currentStep = value - min / step;
+    let viewStep = currentStep + STEP_OFFSET;
+    let x = viewStep * STEP_SIZE * -1;
+    var sliderStyle = {
+			transform: "translate(" + x + "%, 0)"
+		};
 		return (
 			<div className="slider">
-        <div className="slider-wrapper">
+        <div className="slider-wrapper" style={sliderStyle}>
           { steps.map((tempo, i) => 
-            <div className="item">{ tempo }</div>
+            <div className="item">{tempo}</div>
           )}
         </div>
       </div>
