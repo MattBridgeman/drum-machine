5e8c56dcf7b058a6d5f99a7ddba1355553043801
Matthew Bridgeman <mattbridgeman@YachtWorlds-MacBook-Pro.local>
2016-12-09 17:48:45 +0000

    Attempt at slider tidy up

--- src/js/components/slider/slider.react.jsx
+++ src/js/components/slider/slider.react.jsx
@@ -11,7 +11,7 @@ class Slider extends React.Component {
 	constructor(props) {
 		super(props);
     this.state = {
-      lastTranslate: null,
+      lastTranslate: [0, 0],
       touchData: [],
       coordinateData: {
         numberOfItems: 0,
@@ -22,8 +22,6 @@ class Slider extends React.Component {
       touching: false,
       touchMomentum: 0
     };
-    // lastTranslate = getInitialTranslateData();
-    // render();
 	}
 	
 	render() {
@@ -48,39 +46,49 @@ class Slider extends React.Component {
 		);
 	}
 
+  _render() {
+    window.requestAnimationFrame(() => {
+      var translateData = this.getTranslateData();
+      var x = translateData[0];
+      var y = translateData[1];
+      $wrapper.style.transform = 'translate(' + x + 'px, 0px)';
+      _render();
+    });
+  }
+
   getNow() {
     return new Date().getTime();
   }
 
   initTouchData() {
     this.setState({
-      lastTranslate: getTranslateData,
-      touchData: []
+      lastTranslate: this.getTranslateData(),
+      touchData: [[0,0,0]]
     });
   }
 
   updateTouchData({clientX, clientY}) {
     this.setState({
-      touchData: this.state.touchData.concat([clientX, clientY, getNow()])
+      touchData: this.state.touchData.concat([clientX, clientY, this.getNow()])
     });
   }
 
   getInitialTranslateData() {
-    var x = getMaxX();
-    return [x, 0, getNow()];
+    var x = this.getMaxX();
+    return [x, 0, this.getNow()];
   }
 
   getTranslateData() {
     if(this.state.touchData.length){
-      var position = getPosition();
-      var scrollForceOffset = getScrollForceOffset(position);
-      var stretchedBoundary = applyStretchedBoundary(scrollForceOffset);
+      var position = this.getPosition();
+      var scrollForceOffset = this.getScrollForceOffset(position);
+      var stretchedBoundary = this.applyStretchedBoundary(scrollForceOffset);
       var boundedPosition;
-      if(stretchedBeyondBounds(stretchedBoundary)) {
-        boundedPosition = getScrollBounceback(stretchedBoundary);
+      if(this.stretchedBeyondBounds(stretchedBoundary)) {
+        boundedPosition = this.getScrollBounceback(stretchedBoundary);
       } else {
         //boundedPosition = applyBoundary(stretchedBoundary);
-        boundedPosition = snapToValue(stretchedBoundary);
+        boundedPosition = this.snapToValue(stretchedBoundary);
       }
       return boundedPosition;
     } else {
@@ -90,8 +98,8 @@ class Slider extends React.Component {
 
   getTouchMomentum() {
     var diffMilliseconds = 250;
-    var now = getNow();
-    var recentTouches = touchData
+    var now = this.getNow();
+    var recentTouches = this.state.touchData
       .filter(touch =>
         now - touch[2] <= diffMilliseconds
       )
@@ -108,19 +116,19 @@ class Slider extends React.Component {
   }
 
   getPosition() {
-    var first = _.first(touchData);
-    var last = _.last(touchData);
+    var first = first(this.state.touchData);
+    var last = last(this.state.touchData);
     var x = this.state.lastTranslate[0]-(first[0]-last[0]);
     var y = this.state.lastTranslate[1]-(first[1]-last[1]);
     return [x,y];
   }
 
-  getScrollForceOffset ([x,y]) {
-    if(touching || !touchMomentum) return [x,y];
-    var elapsedTime = getElapsedTimeSinceLastTouch();
-    var distanceToTravel = getScrollDistanceToTravel();
-    var duration = getScrollDuration();
-    if(!isScrollForceBeingApplied()) {
+  getScrollForceOffset([x,y]) {
+    if(this.state.touching || !this.state.touchMomentum) return [x,y];
+    var elapsedTime = this.getElapsedTimeSinceLastTouch();
+    var distanceToTravel = this.getScrollDistanceToTravel();
+    var duration = this.getScrollDuration();
+    if(!this.isScrollForceBeingApplied()) {
       x += distanceToTravel;
       return [x,y];
     }
@@ -128,11 +136,11 @@ class Slider extends React.Component {
     return [x,y];
   }
 
-  getScrollBounceback ([x, y]) {
-    if(touching || isScrollForceBeingApplied()) return [x,y];
-    var timeSinceLastTouch = getElapsedTimeSinceLastTouch();
-    var elapsedTime = timeSinceLastTouch - getScrollDuration();
-    var xBound = normaliseValue(x, getMinX(), getMaxX());
+  getScrollBounceback([x, y]) {
+    if(this.state.touching || this.isScrollForceBeingApplied()) return [x,y];
+    var timeSinceLastTouch = this.getElapsedTimeSinceLastTouch();
+    var elapsedTime = timeSinceLastTouch - this.getScrollDuration();
+    var xBound = normaliseValue(x, this.getMinX(), this.getMaxX());
     var distanceToTravel = x - xBound;
     var duration = 300;
     if(elapsedTime >= duration) {
@@ -143,11 +151,11 @@ class Slider extends React.Component {
     return [x,y];
   }
 
-  snapToValue ([x, y]) {
-    if(touching || isScrollForceBeingApplied()) return [x,y];
-    var timeSinceLastTouch = getElapsedTimeSinceLastTouch();
-    var elapsedTime = timeSinceLastTouch - getScrollDuration();
-    var nearest = nearestValue(x);
+  snapToValue([x, y]) {
+    if(this.state.touching || this.isScrollForceBeingApplied()) return [x,y];
+    var timeSinceLastTouch = this.getElapsedTimeSinceLastTouch();
+    var elapsedTime = timeSinceLastTouch - this.getScrollDuration();
+    var nearest = this.nearestValue(x);
     var distanceToTravel = x - nearest;
     var duration = 200;
     if(elapsedTime >= duration) {
@@ -158,9 +166,9 @@ class Slider extends React.Component {
     return [x,y];
   }
 
-  getElapsedTimeSinceLastTouch () {
-    var last = _.last(touchData);
-    var now = getNow();
+  getElapsedTimeSinceLastTouch() {
+    var last = last(this.state.touchData);
+    var now = this.getNow();
     var elapsedTime = now - last[2];
     return elapsedTime;
   }
@@ -169,41 +177,41 @@ class Slider extends React.Component {
     return touchMomentum * 80;
   }
 
-  getScrollDuration () {
-    var duration = getScrollDistanceToTravel() * 2;
+  getScrollDuration() {
+    var duration = this.getScrollDistanceToTravel() * 2;
     return Math.sqrt(duration * duration);
   }
 
-  isScrollForceBeingApplied () {
-    if(touching || !touchMomentum) return false;
-    var elapsedTime = getElapsedTimeSinceLastTouch();
-    var duration = getScrollDuration();
+  isScrollForceBeingApplied() {
+    if(this.state.touching || !this.state.touchMomentum) return false;
+    var elapsedTime = this.getElapsedTimeSinceLastTouch();
+    var duration = this.getScrollDuration();
     return elapsedTime <= duration;
   }
 
-  applyBoundary ([x,y]) {
-    var xMin = getMinX();
-    var xMax = getMaxX();
+  applyBoundary([x,y]) {
+    var xMin = this.getMinX();
+    var xMax = this.getMaxX();
     var x = normaliseValue(x, xMin, xMax);
     return [x, y];
   }
 
-  applyStretchedBoundary ([x,y]) {
-    var xMin = getMinX();
-    var xMax = getMaxX();
+  applyStretchedBoundary([x,y]) {
+    var xMin = this.getMinX();
+    var xMax = this.getMaxX();
     var x = normalisedStretchValue(x, xMin, xMax);
     return [x, y];
   }
 
-  stretchedBeyondBounds ([x,y]) {
-    var xMin = getMinX();
-    var xMax = getMaxX();
-    return isBeyondNormalisedValue(x, xMin, xMax);
+  stretchedBeyondBounds([x,y]) {
+    var xMin = this.getMinX();
+    var xMax = this.getMaxX();
+    return this.isBeyondNormalisedValue(x, xMin, xMax);
   }
 
-  nearestValue (x) {
+  nearestValue(x) {
     return $items
-      .map(($item, i) => coordinateData.itemWidth * i)
+      .map(($item, i) => this.state.coordinateData.itemWidth * i)
       .reduce((prev, curr) => {
         var prevDiff = prev - x;
         var currDiff = curr - x;
@@ -215,49 +223,45 @@ class Slider extends React.Component {
   }
 
   getMinX(){
-    return -coordinateData.itemWidth * (coordinateData.numberOfItems - 2);
+    return -this.state.coordinateData.itemWidth * (this.state.coordinateData.numberOfItems - 2);
   }
 
   getMaxX(){
-    return coordinateData.itemWidth; 
+    return this.state.coordinateData.itemWidth; 
   }
 
-  _render() {
-    window.requestAnimationFrame(() => {
-      var translateData = getTranslateData();
-      var x = translateData[0];
-      var y = translateData[1];
-      $wrapper.style.transform = 'translate(' + x + 'px, 0px)';
-      _render();
-    });
-  }
-
-  componentDidMount(){
+  componentDidMount() {
 		let { slider: $slider } = this.refs;
 
     $slider.addEventListener('touchstart', e => {
       e.preventDefault();
       var touches = e.touches[0];
-      initTouchData();
-      touching = true;
-      updateTouchData(touches);
+      this.initTouchData();
+      this.setState({
+        touching: true
+      });
+      this.updateTouchData(touches);
     });
 
     $slider.addEventListener('touchmove', e => {
       e.preventDefault();
       var touches = e.touches[0];
-      updateTouchData(touches);
+      this.updateTouchData(touches);
     });
 
     $slider.addEventListener('touchend', e => {
       e.preventDefault();
-      touching = false;
-      var lastTouch = _.last(touchData);
-      updateTouchData({
+      this.setState({
+        touching: true
+      });
+      var lastTouch = last(this.state.touchData);
+      this.updateTouchData({
         clientX: lastTouch[0],
         clientY: lastTouch[1]
       });
-      touchMomentum = getTouchMomentum();
+      this.setState({
+        touchMomentum: this.getTouchMomentum()
+      });
     });
   }
 }
