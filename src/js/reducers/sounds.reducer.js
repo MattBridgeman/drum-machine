dfe9ea62e07130e6b3e529b03e950ac6af6e9566
Matthew Bridgeman <mattbridgeman@YachtWorlds-MacBook-Pro.local>
2016-12-12 16:35:47 +0000

    Add short names to sounds

--- src/js/reducers/sounds.reducer.js
+++ src/js/reducers/sounds.reducer.js
@@ -3,38 +3,47 @@
 const initialState = {
 	0: {
 		name: "Bass Drum",
+		shortName: "BD",
 		path: "samples/909-2/bd01.WAV"
 	},
 	1: {
 		name: "Snare Drum",
+		shortName: "SD",
 		path: "samples/909-2/sd01.WAV"
 	},
 	2: {
 		name: "Low Tom",
+		shortName: "LT",
 		path: "samples/909-2/lt01.WAV"
 	},
 	3: {
 		name: "Mid Tom",
+		shortName: "MT",
 		path: "samples/909-2/mt01.WAV"
 	},
 	4: {
 		name: "Hi Tom",
+		shortName: "HT",
 		path: "samples/909-2/ht01.WAV"
 	},
 	5: {
 		name: "Rim Shot",
+		shortName: "RS",
 		path: "samples/909-2/rs01.WAV"
 	},
 	6: {
 		name: "Hand Clap",
+		shortName: "HC",
 		path: "samples/909-2/cp01.WAV"
 	},
 	7: {
 		name: "Hi Hat",
+		shortName: "HH",
 		path: "samples/909-2/hh01.WAV"
 	},
 	8: {
 		name: "Cymbal",
+		shortName: "CY",
 		path: "samples/909-2/rd01.WAV"
 	}
 };
