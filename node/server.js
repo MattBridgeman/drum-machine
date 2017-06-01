0118ac5ba932267df928ea92fefe820876345695
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-06-01 20:15:48 +0100

    Core library updates

--- node/server.js
+++ node/server.js
@@ -1,11 +1,13 @@
 var connect 	= require('connect'),
+	serveStatic = require('serve-static'),
 	fs			= require('fs'),
 	__args		= process.argv,
 	__dir		= __dirname + '/../build/',
 	port 		= 8081;
 
-connect.createServer(
-	connect.static(__dir)
-).listen(port);
+var app = connect();
+
+app.use(serveStatic(__dir));
+app.listen(8081);
 
 module.exports = connect;
