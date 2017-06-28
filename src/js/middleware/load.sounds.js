391a04aca4256dd04faf7c310527b386f5ed3836
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-06-28 08:29:40 +0100

    rename buffer.js

--- src/js/middleware/load.sounds.js
+++ src/js/middleware/load.sounds.js
@@ -1,6 +1,6 @@
 import { NEW_AUDIO_CONTEXT } from "../constants/audio.context.constants";
 import { newSoundBuffers } from "../actions/audio.context.actions";
-import { loadSounds } from "../library/audio-api/buffer";
+import { loadSounds } from "../library/audio-api/load.sounds";
 import { decodeAudioDataArray } from "../library/audio-api/context";
 
 export const supplySoundBuffers = store => next => {
