2740e22696917e614757f61943b9e6003ca57c07
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-07-09 22:33:42 +0100

    skeleton test added

--- src/js/middleware/__tests__/buffer.test.js
+++ src/js/middleware/__tests__/buffer.test.js
@@ -2,17 +2,34 @@ import TestUtils from "react-dom/test-utils";
 import React from "react";
 import { buffer } from "../buffer";
 import { expect } from "chai";
-import { NEW_AUDIO_CONTEXT } from "../../constants/audio.context.constants";
-import { TOGGLE_PLAY_PAUSE } from "../../constants/play.state.constants";
-import { NEW_BUFFER_SEGMENT, CLEAR_BUFFER_SEGMENTS } from "../../constants/buffer.constants";
+import { newAudioContext } from "../../actions/audio.context.actions";
+import { togglePlayPause } from "../../actions/play.state.actions";
+import { newBufferSegment } from "../../actions/buffer.actions";
 import { last } from "../../library/natives/array";
-import { getStubContext } from "../../library/test-helpers/stubs/audio.api";
-import { GlobalSetTimeout, GlobalSetTimeoutTick } from "../../library/test-helpers/stubs/set.timeout";
+import { timeout } from "../../library/audio-api/interval";
 import configureTestStore from "../../store/test.store";
 import td from "testdouble";
 
 const { renderIntoDocument, Simulate } = TestUtils;
 
 describe("Buffer", () => {
+  //TODO: Figure out how to test life cycle of this middleware
+  it("calls store dispatch with new segments when play starts", () => {
+    // let promise = Promise.resolve(true);
+    // td.replace(timeout, "timeout", () => promise);
+    // let context = {
+    //   currentTime: 1234
+    // };
+    // let store = configureTestStore();
+    // let next = td.function();
+    // let newAction = buffer(store)(next);
+    // newAction(newAudioContext(context));
+    // newAction(togglePlayPause());
+    // td.verify(next(newAudioContext(context)));
+    // td.verify(next(togglePlayPause()));
+    // return promise.then(() => 
+    //   Promise.resolve(true).then(td.verify(next(newBufferSegment({
 
+    // })))));
+  });
 });
