c6de2fdf63b885710fff4719e82ed797c792cd9d
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-07-19 12:26:09 +0100

    Remove obsolete code

--- src/js/library/audio-api/__tests__/interval.test.js
+++ src/js/library/audio-api/__tests__/interval.test.js
@@ -1,32 +1,6 @@
-import Rx from "rxjs/Rx";
 import td from "testdouble";
 import { expect } from "chai";
-import { createIntervalStream, intervalGenerator } from "../interval";
-
-describe("Interval", () => {
-	
-	it("calls adds to the segment stream if current time is greater than interval time", () => {
-		let startTime = 0;
-		let currentTime = 126;
-		let intervalTime = 125;
-		let subject = new Rx.Subject();
-		
-		let segmentStream = createIntervalStream(
-			() => currentTime,
-			() => intervalTime,
-			(callback) => subject.take(1).subscribe(callback)
-		);
-		
-		segmentStream
-			.take(1)
-			.toArray()
-			.subscribe(value => {
-				expect(value.length).to.equal(1);
-			});
-		
-		subject.next(1);
-	});
-});
+import { intervalGenerator } from "../interval";
 
 describe("Interval generator", () => {
 	it("returns the promise from generator whilst 'shouldContinue' is true", () => {
--- src/js/library/audio-api/interval.js
+++ src/js/library/audio-api/interval.js
@@ -1,37 +1,3 @@
-import Rx from "rxjs/Rx";
-
-export var createIntervalStream = (getNow, getIntervalTime, callback, cancelCallback) => Rx.Observable.create(function (observer) {
-	let frameId,
-		loop,
-		tick,
-		prevTime;
-
-	loop = function() {
-		let now = getNow();
-		prevTime = prevTime || now;
-		let deltaTime = now - prevTime;
-		let interval = getIntervalTime() / 1000;
-		let bufferLength = interval / 2;
-
-		if(deltaTime >= bufferLength) {
-			observer.next(prevTime + interval);
-			prevTime = prevTime + interval;
-		}
-		tick();
-	};
-
-	tick = function() {
-		frameId = callback(loop);
-	};
-
-	loop();
-
-    return function () {
-		if(!cancelCallback) return;
-        cancelCallback(frameId);
-    };
-});
-
 export let intervalGenerator = function*(shouldContinue, timeout){
 	while(shouldContinue()){
 		yield timeout();
