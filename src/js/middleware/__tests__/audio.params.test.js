9398c2143c3d8c4d4f770671a4b2c973a1c22ba1
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-07-20 23:38:42 +0100

    Audio params test

--- src/js/middleware/__tests__/audio.params.test.js
+++ src/js/middleware/__tests__/audio.params.test.js
@@ -65,6 +65,112 @@ describe("Audio Params", () => {
     expect(sourceNodes[0].volume.gain.value).to.equal(1);
     expect(sourceNodes[0].master.gain.value).to.equal(1);
   });
+  it("mutes channel", () => {
+    
+    let next = td.function();
+    let state = {
+      channels: [{
+        mute: true,
+        solo: false,
+        pan: 50,
+        volume: 100
+      }],
+      reverb: {
+        decay: 100,
+        seconds: 100
+      }
+    };
+    let mockStore = {
+      getState: () => state
+    };
+    let sourceNodes = [{
+      master: {
+        gain: {
+          value: 0
+        }
+      },
+      volume: {
+        gain: {
+          value: 0
+        }
+      },
+      pan: {
+        setPosition: td.function()
+      },
+      reverbNode: {
+        decay: 0,
+        seconds: 0
+      }
+    }];
+    let nextAction = updateAudioParams(mockStore)(next);
+    nextAction(newSourceNodes(sourceNodes));
+    expect(sourceNodes[0].master.gain.value).to.equal(0);
+  });
+  it("solos a single channel, muting the other", () => {
+    
+    let next = td.function();
+    let state = {
+      channels: [{
+        mute: false,
+        solo: false,
+        pan: 50,
+        volume: 100
+      },{
+        mute: false,
+        solo: true,
+        pan: 50,
+        volume: 100
+      }],
+      reverb: {
+        decay: 100,
+        seconds: 100
+      }
+    };
+    let mockStore = {
+      getState: () => state
+    };
+    let sourceNodes = [{
+      master: {
+        gain: {
+          value: 0
+        }
+      },
+      volume: {
+        gain: {
+          value: 0
+        }
+      },
+      pan: {
+        setPosition: td.function()
+      },
+      reverbNode: {
+        decay: 0,
+        seconds: 0
+      }
+    },{
+      master: {
+        gain: {
+          value: 0
+        }
+      },
+      volume: {
+        gain: {
+          value: 0
+        }
+      },
+      pan: {
+        setPosition: td.function()
+      },
+      reverbNode: {
+        decay: 0,
+        seconds: 0
+      }
+    }];
+    let nextAction = updateAudioParams(mockStore)(next);
+    nextAction(newSourceNodes(sourceNodes));
+    expect(sourceNodes[0].master.gain.value).to.equal(0);
+    expect(sourceNodes[1].master.gain.value).to.equal(1);
+  });
   it("sets pan to equally left and right", () => {
     let next = td.function();
     let state = {
