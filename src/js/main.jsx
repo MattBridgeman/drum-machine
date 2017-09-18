775aa8fb4a65b8db3121226162ab2c42d3d09c65
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-09-18 07:49:26 +0100

    Add React Router and configure hash routing

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import { Route } from "react-router";
 import DrumMachine from "./components/drum-machine/drum.machine.react.jsx";
 import { Provider } from "react-redux";
 import configureStore from "./store/store";
@@ -8,7 +10,9 @@ const store = configureStore();
 
 ReactDOM.render(
 	<Provider store={store}>
-		<DrumMachine />
+		<HashRouter>
+			<Route path="/" component={DrumMachine} />
+		</HashRouter>
 	</Provider>,
 	document.getElementById("drum-machine")
 );
