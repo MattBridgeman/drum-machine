ecdd8893e9f22c28700f3bc7eaf758d7509e4295
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-09-19 13:58:46 +0100

    Header button styling

--- src/js/components/header/header.react.jsx
+++ src/js/components/header/header.react.jsx
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export let Header = (props) => <div className="header">
-  <Link to="/">Drum Machine</Link>
+  <div className="logo">
+    <h1><Link to="/">Drum Machine</Link></h1>
+  </div>
   {
     props.auth.user ?
     (
       <div className="tray">
-        <Link to="/track/save">Save Track</Link>
-        <Link to="/user/logout">Logout</Link>
+        <Link to="/track/save" className="header-link">Save Track</Link>
+        <Link to="/user/logout" className="header-link">Logout</Link>
       </div>
     ) : (
       <div className="tray">
-        <Link to="/user/login">Login</Link>
-        <Link to="/user/signup">Sign Up</Link>
+        <Link to="/user/login" className="header-link">Login</Link>
+        <Link to="/user/signup" className="header-link">Sign Up</Link>
       </div>
     )
   }
