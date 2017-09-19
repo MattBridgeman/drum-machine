ad87cd515b4b5b47eb86cbe2784a4dd12891097e
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-09-19 17:14:32 +0100

    Change redirect logic to take you back to previous page

--- src/js/components/firebase/auth.react.jsx
+++ src/js/components/firebase/auth.react.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import firebaseui from "firebaseui";
 import firebase from "firebase";
 import { uiConfig } from "../../library/firebase/config";
@@ -7,14 +8,22 @@ let ui;
 
 class Auth extends Component {
   render(){
-    return <div id="firebase-auth"></div>
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    return !this.props.auth.user ? 
+      (<div id="firebase-auth"></div>)
+      :
+      (<Redirect to={from} />)
   }
   componentDidMount(){
-    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    if(this.props.auth.user) return;
     ui = ui || new firebaseui.auth.AuthUI(firebase.auth());
     ui.start('#firebase-auth', {
       ...uiConfig,
-      signInSuccessUrl: from.pathname
+      callbacks: {
+        signInSuccess: (user) => this.setState({
+          signedIn: true
+        })
+      }
     });
   }
 }
