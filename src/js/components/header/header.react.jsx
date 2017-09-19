440c663a2e3e75aed77ff3d59cf2b71b6ceb2f7b
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-09-19 07:49:35 +0100

    Switch header links if logged in

--- src/js/components/header/header.react.jsx
+++ src/js/components/header/header.react.jsx
@@ -3,6 +3,18 @@ import { Link } from "react-router-dom";
 
 export let Header = (props) => <div className="header">
   <Link to="/">Drum Machine</Link>
-  <Link to="/user/login">Login</Link>
-  <Link to="/user/signup">Sign Up</Link>
+  {
+    props.auth.user ?
+    (
+      <div className="tray">
+        <Link to="/track/save">Save Track</Link>
+        <Link to="/user/logout">Logout</Link>
+      </div>
+    ) : (
+      <div className="tray">
+        <Link to="/user/login">Login</Link>
+        <Link to="/user/signup">Sign Up</Link>
+      </div>
+    )
+  }
 </div>;
