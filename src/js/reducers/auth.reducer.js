b0625e8d538b62d9650976fbc3211b397a96d61a
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-03 14:20:07 +0100

    Auth test

--- src/js/reducers/auth.reducer.js
+++ src/js/reducers/auth.reducer.js
@@ -8,7 +8,6 @@ export default function auth(state = initialState, action) {
   switch (action.type) {
     case AUTH_STATE_CHANGE:
       return {
-        ...state,
         user: action.value
       }
     default:
