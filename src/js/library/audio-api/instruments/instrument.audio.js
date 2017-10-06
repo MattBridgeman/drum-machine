efadad2deaacc17476101320b30480e202a86f83
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-06 14:46:04 +0100

    Instrument middleware work

--- src/js/library/audio-api/instruments/instrument.audio.js
+++ src/js/library/audio-api/instruments/instrument.audio.js
@@ -1,1 +1,11 @@
-export let getInstrumentAudio = () => null;
+import { drumMachine } from "./drum.machine";
+
+export let getInstrumentAudio = (instruments) => 
+  instruments.map(instrument => {
+    switch(instrument.type) {
+      case "drumMachine":
+        return drumMachine(instrument);
+      default:
+        return;
+    }
+  });
