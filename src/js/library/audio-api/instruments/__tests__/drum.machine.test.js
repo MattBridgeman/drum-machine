6d58c99c4dfc9f8547cb8b9ba55d41c872b68a9e
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-07 21:35:27 +0100

    solos a single channel

--- src/js/library/audio-api/instruments/__tests__/drum.machine.test.js
+++ src/js/library/audio-api/instruments/__tests__/drum.machine.test.js
@@ -59,4 +59,33 @@ describe("Drum Machine", () => {
     expect(drumMachine.outputs.channels[0].master.gain.value).to.equal(0);
     td.reset();
   });
+  it("solos a single channel, muting the other", () => {
+    let context = getStubContext();
+    td.replace(_context, "getAudioContext", () => context);
+    let drumMachine = createDrumMachine();
+    let state = {
+      drumMachine: {
+        0: [{
+          mute: false,
+          solo: false,
+          pan: 50,
+          volume: 100
+        },{
+          mute: false,
+          solo: true,
+          pan: 50,
+          volume: 100
+        }]
+      },
+      instruments: [{
+        id: 0,
+        type: "drumMachine",
+        machineId: 0
+      }]
+    };
+    drumMachine.update(state.instruments["0"], state);
+    expect(drumMachine.outputs.channels[0].master.gain.value).to.equal(0);
+    expect(drumMachine.outputs.channels[1].master.gain.value).to.equal(1);
+    td.reset();
+  });
 });
