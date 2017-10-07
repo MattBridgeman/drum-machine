7d4de457e8b945c657fb8429f41df27f64e765f1
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-07 22:13:26 +0100

    Figuring out way to find a connection route

--- src/js/library/audio-api/instruments/connections.js
+++ src/js/library/audio-api/instruments/connections.js
@@ -1,12 +1,21 @@
 export let cache = {};
 
+// instrumentNode = instrument: {
+// machineId: 0  
+//}, machine: {
+//  outputs: {
+//    channels
+//  }
+//}
+
 export let updateConnections = (instrumentNodes, state) => {
   let idCache = {};
   let { connections } = state;
   connections.map(connection => {
     let { id, from, to } = connection;
     let item = cache[id];
-    if(item) return item;
-    
+    if(item) return;
+    // let fromMachine = 
+    // let fromNode = 
   });
 };
