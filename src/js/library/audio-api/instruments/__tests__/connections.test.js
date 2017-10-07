bf6aff84a1d016ab3fb7125e246a57000a79d42b
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-07 22:08:29 +0100

    beginnings of connections

---
+++ src/js/library/audio-api/instruments/__tests__/connections.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { expect } from "chai";
+import td from "testdouble";
+import { updateConnections } from "../connections";
+
+describe("Connections", () => {
+  it("", () => {
+
+  });
+});
--- src/js/library/audio-api/instruments/connections.js
+++ src/js/library/audio-api/instruments/connections.js
@@ -1,1 +1,12 @@
-export let updateConnections = () => null;
+export let cache = {};
+
+export let updateConnections = (instrumentNodes, state) => {
+  let idCache = {};
+  let { connections } = state;
+  connections.map(connection => {
+    let { id, from, to } = connection;
+    let item = cache[id];
+    if(item) return item;
+    
+  });
+};
