8e968349f889d138ebba47fffff718938fe9fa91
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-19 07:27:48 +0100

    Test fix

--- src/js/reducers/__tests__/track.test.js
+++ src/js/reducers/__tests__/track.test.js
@@ -3,7 +3,9 @@ import track from "../track.reducer";
 
 describe("Track reducer", () => {
 	it("returns the default track state", () => {
-    let state = track();
+    let state = track(undefined, {
+      type: "SOME_RANDOM_ACTION"
+    });
     expect(state.name).to.equal("Untitled Track");
     expect(state.state).to.equal("preload");
   });
