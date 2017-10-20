a8e899f610fe8e4caaebe8147d57b9d4f14e1e84
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-20 20:04:58 +0100

    Work to detect new track route

--- src/js/components/views/track.react.jsx
+++ src/js/components/views/track.react.jsx
@@ -3,12 +3,38 @@ import { connect } from "react-redux";
 import { Instrument } from "../instrument/instrument.react.jsx";
 import { View } from "./view.react.jsx";
 
-let Track = (props) =>
-  <View {...props} view={{ name: "track" }}>
-    <div className="container">
-      <Instrument {...props} />
-    </div>
-  </View>;
+class Track extends Component {
+  render(){
+    let { props } = this;
+    return <View {...props} view={{ name: "track" }}>
+      <div className="container">
+        <Instrument {...props} />
+      </div>
+    </View>;
+  }
+	componentWillReceiveProps(nextProps){
+		let { pathname } = this.props.location;
+		let { pathname: nextPathname } = nextProps.location;
+    if(this.isNewPath(pathname, nextPathname)
+      && this.matchesTrackRoute(this.props.match.path)
+      && this.isNewTrack(nextProps.props.match.params.trackId)) {
+      //fire off an action
+    }
+	}
+	componentDidMount(){
+		alert("TestTrackRoute: mount");
+  }
+  isNewPath(oldPath, newPath) {
+
+  }
+  isNewTrack(trackId = "default"){
+    
+  }
+  matchesTrackRoute(route){
+    return route === "/users/:userId/tracks/:trackId"
+      || route === "/";
+  }
+};
 
 function mapStateToProps(state) {
 	//return state.drumMachine;
