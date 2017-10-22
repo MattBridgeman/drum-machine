e2031b03d04f67c2706f0d18394f139f2b389c45
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-22 18:23:50 +0100

    Test fix

--- src/js/reducers/reverb.reducer.js
+++ src/js/reducers/reverb.reducer.js
@@ -34,6 +34,8 @@ export default function reverb(state = initialState, action) {
 					decay: action.value
 				}
 			};
+		case NEW_TRACK_LOADING:
+			return initialState;
 		default:
 			return state;
 	}
