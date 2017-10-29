48add172da926793a337f0ee023913f3d38d1f24
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-10-29 08:43:22 +0000

    Track middleware loads track default

--- src/js/middleware/__tests__/track.test.js
+++ src/js/middleware/__tests__/track.test.js
@@ -2,6 +2,9 @@ import React from "react";
 import { expect } from "chai";
 import td from "testdouble";
 import { track } from "../track";
+import { timeout } from "../../library/audio-api/interval";
+import { NEW_TRACK_LOADING } from "../../constants/track.constants";
+import { loadDefaultTrack } from "../../actions/track.actions";
 import configureTestStore from "../../store/test.store";
 
 describe("Track", () => {
@@ -12,5 +15,22 @@ describe("Track", () => {
     newAction({type: "A_RANDOM_ACTION"});
     td.verify(next({type: "A_RANDOM_ACTION"}));
     td.reset();
+  });
+  it("calls action of type loadDefaultTrack if trackId is default", () => {
+    let then = (cb) => cb();
+    let get = () => ({ then });
+    td.replace(timeout, "get", get);
+    let context = {
+      currentTime: 1234
+    };
+    let store = configureTestStore();
+    let next = td.function();
+    let newAction = track(store)(next);
+    newAction({
+      type: NEW_TRACK_LOADING,
+      userId: undefined,
+      trackId: "default"
+    });
+    td.verify(next(loadDefaultTrack()));
   });
 });
