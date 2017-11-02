c676b5baac231edb54f0c48c1b58a5474c1d22e8
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-11-02 08:01:14 +0000

    Tests for loading default track

--- src/js/middleware/__tests__/track.test.js
+++ src/js/middleware/__tests__/track.test.js
@@ -3,8 +3,8 @@ import { expect } from "chai";
 import td from "testdouble";
 import { track } from "../track";
 import { timeout } from "../../library/audio-api/interval";
-import { NEW_TRACK_LOADING } from "../../constants/track.constants";
-import { loadDefaultTrack } from "../../actions/track.actions";
+import { NEW_TRACK_LOADING, LOAD_DEFAULT_TRACK } from "../../constants/track.constants";
+import { loadDefaultTrack, newTrackLoading } from "../../actions/track.actions";
 import configureTestStore from "../../store/test.store";
 
 describe("Track", () => {
@@ -16,22 +16,50 @@ describe("Track", () => {
     td.verify(next({type: "A_RANDOM_ACTION"}));
     td.reset();
   });
-  it("calls action of type loadDefaultTrack if trackId is default", () => {
-    let get = () => ({
-      then: cb => cb()
-    });
+  it("calls action of type newTrackLoading and loadDefaultTrack if no track is loaded and there's no track id", () => {
+    let get = cb => {
+      let ret = {
+        then: cb => {
+          cb();
+          return ret;
+        }
+      }
+      return ret;
+    };
     td.replace(timeout, "get", get);
     let context = {
       currentTime: 1234
     };
-    let store = configureTestStore();
+    let state = {
+      router: {
+        location: {
+          pathname: "/"
+        }
+      },
+      track: {
+        trackId: undefined,
+        state: "idle"
+      }
+    };
+    let store = {
+      getState: () => state
+    }
     let next = td.function();
     let newAction = track(store)(next);
     newAction({
-      type: NEW_TRACK_LOADING,
-      userId: undefined,
-      trackId: "default"
+      type: "A_RANDOM_ACTION"
     });
-    td.verify(next(loadDefaultTrack()));
+    td.verify(next({
+      type: "A_RANDOM_ACTION"
+    }));
+    td.verify(next({
+      type: NEW_TRACK_LOADING,
+      trackId: "default",
+      userId: undefined
+    }));
+    td.verify(next({
+      type: LOAD_DEFAULT_TRACK
+    }));
+    td.reset();
   });
 });
