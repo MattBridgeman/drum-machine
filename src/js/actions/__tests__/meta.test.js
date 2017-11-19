3e49d1e77c6415170cc576e7c2e53dcf53dbc6a5
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-11-19 16:38:09 +0000

    Meta actions

--- src/js/actions/__tests__/meta.test.js
+++ src/js/actions/__tests__/meta.test.js
@@ -1,12 +1,33 @@
 import { expect } from "chai";
-import { changeTrackTitle } from "../meta.actions";
-import { CHANGE_TRACK_TITLE } from "../../constants/meta.constants";
+import { changeTrackTitle, changeTrackCreatedDate, changeTrackUpdatedDate } from "../meta.actions";
+import { CHANGE_TRACK_TITLE, CHANGE_TRACK_CREATED_DATE, CHANGE_TRACK_UPDATED_DATE } from "../../constants/meta.constants";
 
-describe("Title actions", function() {
+describe("Meta actions", function() {
 
 	it("changeTrackTitle returns corresponding action", function() {
-		var action = changeTrackTitle({
-			title: "Untitled Track"
+		var action = changeTrackTitle("Untitled Track");
+
+    expect(action).to.deep.equal({
+      type: CHANGE_TRACK_TITLE,
+      title: "Untitled Track"
+    });
+  });
+
+	it("changeTrackCreatedDate returns corresponding action", function() {
+		var action = changeTrackCreatedDate(1234);
+    
+    expect(action).to.deep.equal({
+      type: CHANGE_TRACK_CREATED_DATE,
+      createdDate: 1234
+    });
+  });
+  
+  it("changeTrackUpdatedDate returns corresponding action", function() {
+    var action = changeTrackUpdatedDate(1234);
+    
+    expect(action).to.deep.equal({
+      type: CHANGE_TRACK_UPDATED_DATE,
+      updatedDate: 1234
     });
   });
 });
