fa8780d3f49241b650e35e788d7d600fac5b2339
Matthew Bridgeman <matthew.bridgeman@boats.com>
2017-12-21 07:57:42 +0000

    Tracks loading and loaded in reducer

--- src/js/reducers/tracks.reducer.js
+++ src/js/reducers/tracks.reducer.js
@@ -1,15 +1,30 @@
 import { NEW_TRACK_LOADING, LOAD_DEFAULT_TRACK, NEW_TRACK_SAVE, NEW_TRACK_LOADED } from "../constants/track.constants";
+import { USER_TRACKS_LOADING, USER_TRACKS_LOADED } from "../constants/tracks.constants";
 
 //Tracks States: "idle", "loading"
 
 let defaultState = {
   state: "idle",
-  userId: undefined,
-  tracks: []
+  tracks: {
+    //userId: []
+  }
 };
 
 export default function tracks(state = defaultState, action) {
   switch (action.type) {
+    case USER_TRACKS_LOADING:
+      return {
+        ...state,
+        state: "loading"
+      }
+    case USER_TRACKS_LOADED:
+      return {
+        state: "idle",
+        tracks: {
+          ...state.tracks,
+          [action.userId]: action.tracks
+        }
+      }
     default:
       return state;
   }
