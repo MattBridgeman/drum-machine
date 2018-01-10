b4b1721db2d2371beb4226646e9e72369a2193a9
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-10 08:45:17 +0000

    Samples drag and drop work

--- src/js/components/views/user/samples.react.jsx
+++ src/js/components/views/user/samples.react.jsx
@@ -9,30 +9,56 @@ import { objectToArrayWithKeyValue } from "../../../library/natives/array";
 import { Modal } from "../../modal/modal.react.jsx";
 import { Maybe } from "../../maybe/maybe.react.jsx";
 
+class UploadSampleModal extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dragover: false
+    };
+  }
+  onDragOver(e){
+    this.setState({
+      dragover: true
+    });
+  }
+  onDragLeave(e){
+    this.setState({
+      dragover: false
+    });
+  }
+  render(){
+    let { samples, match, auth } = this.props;
+    let userId = getValueFromPath(match, "params/userId");
+    let currentUserId = getValueFromPath(auth, "user/uid");
+    let { dragover } = this.state;
+
+    return <Maybe condition={userId === currentUserId}>
+      <div className="upload-a-sample">
+        <Modal title="Upload a sample" text="Upload a sample">
+          {
+            props => (
+              <div className={"upload" + ( dragover ? " highlight" : "")} onDragOver={e => this.onDragOver(e)} onDragLeave={e => this.onDragLeave(e)}>
+                <span className="upload-cta">Drag a file to upload here</span>
+                <label className="upload-label" htmlFor="upload">or upload a file</label>
+                <input  className="upload-input" type="file" id="upload" />
+              </div>
+            )
+          }
+        </Modal>
+      </div>
+    </Maybe>
+  }
+}
+
 class SamplesList extends Component {
   render(){
     let { samples, match, auth } = this.props;
     let userId = getValueFromPath(match, "params/userId");
     let userSamples = getValueFromPath(samples, `samples/${userId}`) || {};
-    let currentUserId = getValueFromPath(auth, "user/uid");
     let userSamplesList = objectToArrayWithKeyValue(userSamples);
     return <div className="large-list">
       <h2>Samples</h2>
-      <Maybe condition={userId === currentUserId}>
-        <div className="upload-a-sample">
-          <Modal title="Upload a sample" text="Upload a sample">
-            {
-              props => (
-                <div className="upload">
-                  <span className="upload-cta">Drag a file to upload here</span>
-                  <label className="upload-label" htmlFor="upload">or upload a file</label>
-                  <input  className="upload-input" type="file" id="upload" />
-                </div>
-              )
-            }
-          </Modal>
-        </div>
-      </Maybe>
+      <UploadSampleModal {...this.props} />
       <Maybe condition={userSamplesList.length}>
         <ul>
           {
