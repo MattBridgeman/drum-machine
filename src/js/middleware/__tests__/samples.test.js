1549d5539d135cb19804c04d36ff07307a9ff302
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-23 08:48:34 +0000

    Failing test case

--- src/js/middleware/__tests__/samples.test.js
+++ src/js/middleware/__tests__/samples.test.js
@@ -1,8 +1,11 @@
 import { samplesMiddleware } from "../samples";
 import { expect } from "chai";
+import td from "testdouble";
 import { timeout } from "../../library/audio-api/interval";
 import configureTestStore from "../../store/test.store";
-import td from "testdouble";
+import * as db from "../../library/firebase/db";
+import { getPromiseMock } from "../../library/test-helpers/mocks/promise";
+import { UPLOAD_SAMPLE, SAMPLE_UPLOADING, SAMPLE_UPLOADED } from "../../constants/samples.constants";
 
 describe("Samples", () => {
   it("passes 'next' onwards for all action types", () => {
@@ -11,5 +14,75 @@ describe("Samples", () => {
     let newAction = samplesMiddleware(store)(next);
     newAction({type: "A_RANDOM_ACTION"});
     td.verify(next({type: "A_RANDOM_ACTION"}));
+  });
+
+  it("calls sampleUploading, uploadUserSample then newSampleUploaded on UPLOAD_SAMPLE action", () => {
+    let sample = {
+      name: "sample name",
+      shortName: "SN"
+    };
+    let { promise, flush } = getPromiseMock(sample);
+    let get = cb => {
+      let ret = {
+        then: cb => {
+          cb();
+          return ret;
+        }
+      }
+      return ret;
+    };
+
+    //mocks
+    let uploadUserSample = td.function();
+    td.when(uploadUserSample(td.matchers.anything(), td.matchers.anything())).thenReturn(promise);
+
+    td.replace(timeout, "get", get);
+    td.replace(db, "uploadUserSample", uploadUserSample);
+    let state = {
+      router: {
+        location: {
+          pathname: "/users/123/tracks/234"
+        }
+      },
+      track: {
+        trackId: "234",
+        state: "idle"
+      },
+      auth: {
+        user: null
+      }
+    };
+    let store = {
+      getState: () => state
+    }
+    let next = td.function();
+    let newAction = samplesMiddleware(store)(next);
+    
+    newAction({
+      type: UPLOAD_SAMPLE,
+      name: "sample name",
+      shortName: "SN",
+      file: {
+        name: "samplename.wav"
+      }
+    });
+    flush();
+
+    td.verify(next({
+      type: UPLOAD_SAMPLE,
+      name: "sample name",
+      shortName: "SN",
+      file: {
+        name: "samplename.wav"
+      }
+    }));
+    td.verify(next({
+      type: SAMPLE_UPLOADING
+    }));
+    td.verify(next({
+      type: SAMPLE_UPLOADED,
+      ...sample
+    }));
+    td.reset();
   });
 });
