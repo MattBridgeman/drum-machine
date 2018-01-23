f47d50bca222f185698456f24a64d747df0aeade
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-23 15:35:01 +0000

    Passing tests for sample delete flow

--- src/js/middleware/__tests__/samples.test.js
+++ src/js/middleware/__tests__/samples.test.js
@@ -5,7 +5,7 @@ import { timeout } from "../../library/audio-api/interval";
 import configureTestStore from "../../store/test.store";
 import * as db from "../../library/firebase/db";
 import { getPromiseMock } from "../../library/test-helpers/mocks/promise";
-import { UPLOAD_SAMPLE, SAMPLE_UPLOADING, SAMPLE_UPLOADED } from "../../constants/samples.constants";
+import { UPLOAD_SAMPLE, SAMPLE_UPLOADING, SAMPLE_UPLOADED, SAMPLE_DELETED, DELETE_SAMPLE } from "../../constants/samples.constants";
 
 describe("Samples", () => {
   it("passes 'next' onwards for all action types", () => {
@@ -94,6 +94,72 @@ describe("Samples", () => {
       createdDate: undefined,
       sampleId: undefined,
       userId: "123"
+    }));
+    td.reset();
+  });
+
+  it("calls deleteUserSample then sampleDeleted on DELETE_SAMPLE action", () => {
+    let sample = {
+      name: "sample name",
+      shortName: "SN",
+      userId: "123"
+    };
+    let { promise, flush } = getPromiseMock(sample);
+    let get = cb => {
+      let ret = {
+        then: cb => {
+          cb();
+          return ret;
+        }
+      }
+      return ret;
+    };
+
+    //mocks
+    let deleteUserSample = td.function();
+    td.when(deleteUserSample(td.matchers.anything(), td.matchers.anything(), td.matchers.anything())).thenReturn(promise);
+
+    td.replace(timeout, "get", get);
+    td.replace(db, "deleteUserSample", deleteUserSample);
+    let state = {
+      router: {
+        location: {
+          pathname: "/users/123/tracks/234"
+        }
+      },
+      track: {
+        trackId: "234",
+        state: "idle"
+      },
+      auth: {
+        user: {
+          uid: "123"
+        }
+      },
+      samples: {
+        upload: {
+          state: "idle"
+        }
+      }
+    };
+    let store = {
+      getState: () => state
+    }
+    let next = td.function();
+    let newAction = samplesMiddleware(store)(next);
+    
+    newAction({
+      type: DELETE_SAMPLE,
+      name: "sample name",
+      shortName: "SN",
+      userId: "123",
+      id: "234"
+    });
+    flush();
+    td.verify(next({
+      type: SAMPLE_DELETED,
+      userId: "123",
+      id: "234"
     }));
     td.reset();
   });
