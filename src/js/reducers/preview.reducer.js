0699e2e935e8b1879cc696829c4de9f1df49d842
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-24 07:26:57 +0000

    Skeleton preview reducer

---
+++ src/js/reducers/preview.reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  playing: true,
+  soundId: "asdsADSDasdsd"
+};
+
+export default function preview(state = initialState, action) {
+  switch(action.type){
+    default:
+      return state;
+  }
+};
