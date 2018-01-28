30fb47baca3c41f4493199af21092fd4657c9dff
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-28 20:02:36 +0000

    Envelope work

--- src/js/components/synth/synth.react.jsx
+++ src/js/components/synth/synth.react.jsx
@@ -9,6 +9,7 @@ let Synth = props => {
         <h3>Oscillator 1</h3>
         <div className="oscillator-controls">
           <div className="radio-slider">
+            <h3 className="item-label">Type</h3>
             Sine
           </div>
           <Slider name="Octave" min={0} max={7} step={1} value={0} onValueChange={ value => false } />
@@ -21,6 +22,7 @@ let Synth = props => {
         <h3>Oscillator 2</h3>
         <div className="oscillator-controls">
           <div className="radio-slider">
+            <h3 className="item-label">Type</h3>
             Square
           </div>
           <Slider name="Octave" min={0} max={7} step={1} value={0} onValueChange={ value => false } />
@@ -33,7 +35,24 @@ let Synth = props => {
     <div className="envelopes">
       <div className="envelope">
         <h3>Amp Envelope</h3>
-
+        <div className="envelope-controls">
+          <div className="vertical-range">
+            <label htmlFor="" className="item-label">Attack</label>
+            <input id="" type="range" min="0" max="100" value="0" step="1" />
+          </div>
+          <div className="vertical-range">
+            <label htmlFor="" className="item-label">Decay</label>
+            <input id="" type="range" min="0" max="100" value="0" step="1" />
+          </div>
+          <div className="vertical-range">
+            <label htmlFor="" className="item-label">Sustain</label>
+            <input id="" type="range" min="0" max="100" value="0" step="1" />
+          </div>
+          <div className="vertical-range">
+            <label htmlFor="" className="item-label">Release</label>
+            <input id="" type="range" min="0" max="100" value="0" step="1" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
