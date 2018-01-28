089d22e0544475b8bb47065751278e670984cd53
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-28 19:46:25 +0000

    Synth style work

--- src/js/components/synth/synth.react.jsx
+++ src/js/components/synth/synth.react.jsx
@@ -4,12 +4,25 @@ import { Slider } from "../slider/slider.react.jsx";
 
 let Synth = props => {
   return <div className="synth-machine">
-    <div className="oscillator">
-      <h3>Oscillator 1</h3>
-      <Slider name="Octave" min={0} max={7} step={1} value={0} onValueChange={ value => false } />
-      <Slider name="Semitone" min={0} max={11} step={1} value={0} onValueChange={ value => false } />
-      <Slider name="Cent" min={0} max={100} step={1} value={0} onValueChange={ value => false } />
-      <Rotator name="Velocity" value={0} onValueChange={ value => false } />
+    <div className="oscillator-container">
+      <div className="oscillator">
+        <h3>Oscillator 1</h3>
+        <div className="oscillator-controls">
+          <Slider name="Octave" min={0} max={7} step={1} value={0} onValueChange={ value => false } />
+          <Slider name="Semitone" min={0} max={11} step={1} value={0} onValueChange={ value => false } />
+          <Slider name="Cent" min={0} max={100} step={1} value={0} onValueChange={ value => false } />
+          <Rotator name="Velocity" value={0} onValueChange={ value => false } />
+        </div>
+      </div>
+      <div className="oscillator">
+        <h3>Oscillator 2</h3>
+        <div className="oscillator-controls">
+          <Slider name="Octave" min={0} max={7} step={1} value={0} onValueChange={ value => false } />
+          <Slider name="Semitone" min={0} max={11} step={1} value={0} onValueChange={ value => false } />
+          <Slider name="Cent" min={0} max={100} step={1} value={0} onValueChange={ value => false } />
+          <Rotator name="Velocity" value={0} onValueChange={ value => false } />
+        </div>
+      </div>
     </div>
   </div>
 };
