716b824ec20e7d9401715db95f7d1fc99525fe0e
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-28 19:36:38 +0000

    Synth work

--- src/js/components/synth/synth.react.jsx
+++ src/js/components/synth/synth.react.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { Rotator } from "../rotator/rotator.react.jsx";
+import { Slider } from "../slider/slider.react.jsx";
 
 let Synth = props => {
   return <div className="synth-machine">
-    synth
+    <div className="oscillator">
+      <h3>Oscillator 1</h3>
+      <Slider name="Octave" min={0} max={7} step={1} value={0} onValueChange={ value => false } />
+      <Slider name="Semitone" min={0} max={11} step={1} value={0} onValueChange={ value => false } />
+      <Slider name="Cent" min={0} max={100} step={1} value={0} onValueChange={ value => false } />
+      <Rotator name="Velocity" value={0} onValueChange={ value => false } />
+    </div>
   </div>
 };
 
