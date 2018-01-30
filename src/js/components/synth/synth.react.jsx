927d693129b0c596cf5da317b793f19f1a034104
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-30 07:30:05 +0000

    Add more controls

--- src/js/components/synth/synth.react.jsx
+++ src/js/components/synth/synth.react.jsx
@@ -4,6 +4,11 @@ import { Slider } from "../slider/slider.react.jsx";
 
 let Synth = props => {
   return <div className="synth-machine">
+    <div className="basic-controls">
+      <Slider name="Voices" min={1} max={8} step={1} value={1} onValueChange={ value => false } />
+      <Rotator name="Volume" value={0} onValueChange={ value => false } />
+      <Rotator name="Pan" value={0} onValueChange={ value => false } />
+    </div>
     <div className="oscillator-container">
       <div className="oscillator">
         <h3>Oscillator 1</h3>
@@ -30,6 +35,17 @@ let Synth = props => {
           <Slider name="Cent" min={0} max={100} step={1} value={0} onValueChange={ value => false } />
           <Rotator name="Velocity" value={0} onValueChange={ value => false } />
         </div>
+      </div>
+    </div>
+    <div className="filter">
+      <h3>Filter</h3>
+      <div className="vertical-range">
+        <label htmlFor="filter-frequency" className="item-label">Frequency</label>
+        <input id="filter-frequency" type="range" min="0" max="100" value="0" step="1" />
+      </div>
+      <div className="vertical-range">
+        <label htmlFor="filter-resonance" className="item-label">Resonance</label>
+        <input id="filter-resonance" type="range" min="0" max="100" value="0" step="1" />
       </div>
     </div>
     <div className="envelopes">
@@ -75,6 +91,7 @@ let Synth = props => {
           </div>
         </div>
       </div>
+
     </div>
   </div>
 };
