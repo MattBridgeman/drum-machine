fd987c178a6ab3790a127a55b467ad392bee1459
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-01-31 07:50:56 +0000

    synth oscillator created and connected

--- src/js/library/audio-api/instruments/synth.js
+++ src/js/library/audio-api/instruments/synth.js
@@ -3,24 +3,35 @@ import { getAudioContext } from "../context";
 export let createSynth = () => {
 
   let context = getAudioContext();
+  let output = context.createGain();
+  let voices = [];
+  let tmpVoice = null;
 
   let init = () => {
-
+    tmpVoice = context.createOscillator();
+    tmpVoice.type = 'square';
+    tmpVoice.frequency.setValueAtTime(220, 0);
+    tmpVoice.connect(output);
+    //tmpVoice.start();
   };
 
   let update = (instrument, state) => {
     let { machineId } = instrument;
     let { synth } = state;
     let currentSynth = synth[machineId];
-    console.log(currentSynth);
   };
   
   let remove = () => {
     context = null;
   };
 
+  init();
+
   return {
     update,
-    remove
+    remove,
+    outputs: {
+      main: output
+    }
   }
 };
