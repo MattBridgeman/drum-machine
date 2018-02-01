bd9f788ffa99182a915131aa90acc9e20ea65c02
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-02-01 17:36:04 +0000

    synth ideas

--- src/js/library/audio-api/instruments/synth.js
+++ src/js/library/audio-api/instruments/synth.js
@@ -5,6 +5,7 @@ export const MAX_VOICES = 16;
 // Represents the state of the synth as a snapshot in time
 // which will change over time
 let initialState = {
+  time: 0,
   oscilators: {
     osc1: {
       waveType: "sine",
@@ -21,7 +22,6 @@ let initialState = {
       amount: 0
     }
   },
-  fm: true,
   envelopes: {
     amp: {
       attack: 0,
@@ -41,6 +41,7 @@ let initialState = {
 //TODO: action will represent keys pressed state and a time
 //keys should be filtered by the max voices and ordered by startTime
 //keys should persist even after key up
+//but there should only be one key item per note e.g. only one a1
 //the resolution at which the fn is called will determine the "responsiveness" / quality of the envlopes
 //action = {
 //  time: 1234
