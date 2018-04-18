7b6334479af27d3718a7f205971011b043c16eee
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-04-18 13:05:17 +0100

    Lint

--- src/js/middleware/keys.js
+++ src/js/middleware/keys.js
@@ -4,13 +4,13 @@ import { onKeyDown, onKeyUp } from "../actions/keys.actions";
 
 export const keys = store => next => {
   let addEventListener = () => {
-    document.addEventListener('keydown', (event) => {
+    document.addEventListener("keydown", (event) => {
       let { keyCode } = event;
       let keyName = keymap[keyCode];
       let time = new Date().getTime();
       next(onKeyDown(keyCode, keyName, time));
     });
-    document.addEventListener('keydown', (event) => {
+    document.addEventListener("keydown", (event) => {
       let { keyCode } = event;
       let keyName = keymap[keyCode];
       let time = new Date().getTime();
