632f29900b9aac349420238f46d773195398b1b3
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-05-15 19:54:05 +0100

    keys work

--- src/js/reducers/keys.reducer.js
+++ src/js/reducers/keys.reducer.js
@@ -1,15 +1,28 @@
+import { KEYDOWN } from "../constants/keys.constants";
+
 //example entry
 // {
 //   keyCode: "36"
 //   keyName: "home",
-//   keyDownTime: 1234,
-//   keyUpTime: 12345
+//   time: 1234
 // }
 
 let initialState = [];
 
 export default function keys(state = initialState, action) {
+  let { keyCode, keyName, time } = action;
   switch (action.type) {
+    case KEYDOWN:
+      return [
+        {
+          keyCode,
+          keyName,
+          time
+        },
+        ...state.filter(key => key.keyCode !== keyCode)
+      ]
+    case KEYUP:
+      return state.filter(key => key.keyCode !== keyCode)
     default:
       return state;
   }
