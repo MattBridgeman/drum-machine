02fc26d62d456ce6098ae8dc3504254ee473b903
Matthew Bridgeman <matthew.bridgeman@boats.com>
2018-07-23 11:14:17 +0100

    Placeholder keyboard reducer

---
+++ src/js/reducers/__tests__/keyboard.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import keyboard from "../keyboard.reducer";
+
+describe("Keyboard reducer", () => {
+	it("placeholder", () => {
+    
+  });
+});
---
+++ src/js/reducers/keyboard.reducer.js
@@ -0,0 +1,8 @@
+let initialState = {};
+
+export default function keyboard(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
