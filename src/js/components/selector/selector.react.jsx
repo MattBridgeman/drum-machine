6749be8d2afec27ac0ed5587c8cf55b1edf34e00
Matthew Bridgeman <matthew.bridgeman@boats.com>
2019-03-27 22:43:55 +0000

    Increment decrement logic

--- src/js/components/selector/selector.react.jsx
+++ src/js/components/selector/selector.react.jsx
@@ -2,6 +2,36 @@ import React, { Component, Children, cloneElement } from "react";
 import classnames from "classnames";
 
 class Selector extends Component {
+  handleIncrement() {
+    let { onValueChange, children, id } = this.props;
+    let selectedIndex = 0;
+    let childProps = Children.map(children, (child, index) => {
+      if(child.props.selected) {
+        selectedIndex = index;
+      }
+      return child.props;
+    });
+    selectedIndex++;
+    if(selectedIndex >= children.length) {
+      selectedIndex = 0;
+    }
+    onValueChange(childProps[selectedIndex].value);
+  }
+  handleDecrement() {
+    let { onValueChange, children, id } = this.props;
+    let selectedIndex = 0;
+    let childProps = Children.map(children, (child, index) => {
+      if(child.props.selected) {
+        selectedIndex = index;
+      }
+      return child.props;
+    });
+    selectedIndex--;
+    if(selectedIndex < 0) {
+      selectedIndex = children.length - 1;
+    }
+    onValueChange(childProps[selectedIndex].value);
+  }
   render() {
     let { onValueChange, children, id } = this.props;
     return <div className="selector-container">
@@ -10,13 +40,13 @@ class Selector extends Component {
           {children}
         </div>
         <div className="selector-buttons">
-          <button className="button" onClick={() => this.handleIncrement()}>
-            <span className="icon__arrow-up"></span>
-            <span className="assistive">Increment</span>
-          </button>
           <button className="button" onClick={() => this.handleDecrement()}>
-            <span className="icon__arrow-down"></span>
+            <span className="icon__arrow-up"></span>
             <span className="assistive">Decrement</span>
+          </button>
+          <button className="button" onClick={() => this.handleIncrement()}>
+            <span className="icon__arrow-down"></span>
+            <span className="assistive">Increment</span>
           </button>
         </div>
       </div>
