155c4a6ae8fcf06d58d912fb350f9db2a02fecfd
MattBridgeman <matt_bridgeman@hotmail.co.uk>
2019-04-21 07:37:56 +0000

    Hey GitHub, this content is for you! [skip ci]

---
+++ precache-manifest.242c0baab1df57b3be119ddb91d663fa.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4eaba783cd67d7d14c6",
+    "url": "/drum-machine/static/css/main.b5ab116e.chunk.css"
+  },
+  {
+    "revision": "e4eaba783cd67d7d14c6",
+    "url": "/drum-machine/static/js/main.0065f4a4.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "59f4cad0c44acce0a2c5",
+    "url": "/drum-machine/static/css/2.594f742b.chunk.css"
+  },
+  {
+    "revision": "59f4cad0c44acce0a2c5",
+    "url": "/drum-machine/static/js/2.ce622db5.chunk.js"
+  },
+  {
+    "revision": "2f7ea8beac56c7b2cff8a56f8447348f",
+    "url": "/drum-machine/static/media/loading.2f7ea8be.svg"
+  },
+  {
+    "revision": "cd3845f5fda2f69519b1220d8d4094ea",
+    "url": "/drum-machine/static/media/logo.cd3845f5.svg"
+  },
+  {
+    "revision": "e937a78220d31ecc8202b16b97936b09",
+    "url": "/drum-machine/static/media/menu.e937a782.svg"
+  },
+  {
+    "revision": "d245bdf5826cfc084253fddcb69b387b",
+    "url": "/drum-machine/static/media/menu-hover.d245bdf5.svg"
+  },
+  {
+    "revision": "26c905778759da0b14527df91a384c63",
+    "url": "/drum-machine/static/media/close.26c90577.svg"
+  },
+  {
+    "revision": "7390fe8b054aad93808d76972ca98cd7",
+    "url": "/drum-machine/static/media/close-hover.7390fe8b.svg"
+  },
+  {
+    "revision": "a6d8bff04bebef1845302d80261e9abb",
+    "url": "/drum-machine/static/media/folder.a6d8bff0.svg"
+  },
+  {
+    "revision": "4abb5f7dd570601714590d37e2f6f7cb",
+    "url": "/drum-machine/static/media/tick.4abb5f7d.svg"
+  },
+  {
+    "revision": "6f5c98b2cead40a741a97d0d8e3b688b",
+    "url": "/drum-machine/static/media/play.6f5c98b2.svg"
+  },
+  {
+    "revision": "6790a737310b7dc418fef42041654a02",
+    "url": "/drum-machine/static/media/pause.6790a737.svg"
+  },
+  {
+    "revision": "f45a01257ac78d1ed2129cfcd0cf32c4",
+    "url": "/drum-machine/static/media/arrow-up.f45a0125.svg"
+  },
+  {
+    "revision": "44ceecc54448b7d5b1dcfe04b147e973",
+    "url": "/drum-machine/static/media/arrow-down.44ceecc5.svg"
+  },
+  {
+    "revision": "509f61b360f97939f898afd8d0319abe",
+    "url": "/drum-machine/static/media/arrow-down-dark.509f61b3.svg"
+  },
+  {
+    "revision": "861ecc4afd3ddf7be992fb611425f7ef",
+    "url": "/drum-machine/index.html"
+  }
+];
