0e05dd1f46943575d542f0c355ebbc515bcddbe3
Matthew Bridgeman <matthew.bridgeman@boats.com>
2019-04-22 08:06:48 +0100

    play pause button

--- src/js/components/play-bar/playbar.react.jsx
+++ src/js/components/play-bar/playbar.react.jsx
@@ -1,9 +1,30 @@
 import React, { PureComponent } from "react";
+import { Maybe } from "../maybe/maybe.react";
+import { bindActionCreators } from "redux";
+import DrumMachineActions from "../../actions/root.actions";
+
+const PlayBarPlayPause = ({ isPlaying, onClick }) => {
+  return <button class="play-bar__play-pause" onClick={onClick}>
+    <Maybe condition={isPlaying}>
+      <span className="icon icon__pause-light"></span>
+      <span className="play-bar__play-pause__label">Pause</span>
+    </Maybe>
+    <Maybe condition={!isPlaying}>
+      <span className="icon icon__play-light"></span>    
+      <span className="play-bar__play-pause__label">Play</span>  
+    </Maybe>
+  </button>
+};
 
 class PlayBar extends PureComponent {
   render(){
-    let { props } = this;
-    return <div>play bar</div>;
+		const { tempo, playState, dispatch } = this.props;
+		const playStateActions = bindActionCreators(DrumMachineActions.playState, dispatch);
+		const tempoActions = bindActionCreators(DrumMachineActions.tempo, dispatch);
+    const drumMachineActions = bindActionCreators(DrumMachineActions.drumMachine, dispatch);
+    return <div className="play-bar">
+      <PlayBarPlayPause isPlaying={playState.isPlaying} onClick={playStateActions.togglePlayPause} />
+    </div>;
   }
 };
 
