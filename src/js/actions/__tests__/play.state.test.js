cd7630a53fb57a089c05d28a01bc019b19f38d58
Matthew Bridgeman <matthew.bridgeman@boats.com>
2019-04-25 09:44:46 +0100

    actions work

--- src/js/actions/__tests__/play.state.test.js
+++ src/js/actions/__tests__/play.state.test.js
@@ -1,10 +1,26 @@
 import { expect } from "chai";
-import { newBarIndex } from "../play.state.actions";
-import { NEW_BAR_INDEX } from "../../constants/play.state.constants";
+import { play, newBarIndex, pause } from "../play.state.actions";
+import { NEW_BAR_INDEX, PLAY, PAUSE } from "../../constants/play.state.constants";
 
-describe("Tempo actions", function() {
+describe("Play State actions", () => {
 
-	it("Expect newBarIndex to return an new bar index action", function() {
+	it("returns a play action", () => {
+		var action = play();
+
+		expect(action).to.deep.equal({
+			type: PLAY
+		});
+	});
+
+	it("returns a pause action", () => {
+		var action = pause();
+
+		expect(action).to.deep.equal({
+			type: PAUSE
+		});
+	});
+
+	it("Expect newBarIndex to return an new bar index action", () => {
 		var action = newBarIndex(3);
 
 		expect(action).to.deep.equal({
