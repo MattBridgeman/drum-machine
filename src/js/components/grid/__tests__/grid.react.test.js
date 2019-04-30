18c4a1dca65b3c5f9f125fc92f8ab5c39dc56104
Matthew Bridgeman <matthew.bridgeman@boats.com>
2019-04-30 20:33:17 +0100

    Grid test

--- src/js/components/grid/__tests__/grid.react.test.js
+++ src/js/components/grid/__tests__/grid.react.test.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { GridAxis, GridAxisItem } from "../grid.react";
+import { GridAxis, GridAxisItem, Grid, GridRow } from "../grid.react";
 import React from "react";
 import { mount } from "enzyme";
 import td from "testdouble";
@@ -39,11 +39,49 @@ describe("GridAxis", () => {
   });
 });
 
+describe("GridRow", () => {
+	it("renders 2 child items for GridRow x", () => {
+    const wrapper = mount(<GridRow
+      type="x"
+      max={{
+        columns: 2
+      }}
+      offset={{
+        column: 0
+      }}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </GridRow>);
+    expect(wrapper.find(".grid-row-x div").length).to.equal(2);
+  });
+});
+
 describe("GridAxisItem", () => {
 	it("renders 2 child items for GridAxisItem x", () => {
     const wrapper = mount(<GridAxisItem type="x">
       <div></div>
     </GridAxisItem>);
     expect(wrapper.exists(".grid-axis-item-x div")).to.equal(true);
+  });
+});
+
+describe("Grid", () => {
+	it("renders Grid", () => {
+    const wrapper = mount(<Grid
+      max={{
+        columns: 2,
+        rows: 2
+      }}
+      offset={{
+        column: 0,
+        row: 0
+      }}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Grid>);
+    expect(wrapper.find(".grid div").length).to.equal(2);
   });
 });
