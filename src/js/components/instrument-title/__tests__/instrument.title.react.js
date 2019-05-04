fce8e46fa7c106a9aee905537654f6be1322c54f
Matthew Bridgeman <matthew.bridgeman@boats.com>
2019-05-04 19:25:25 +0100

    unit test work

--- src/js/components/instrument-title/__tests__/instrument.title.react.js
+++ src/js/components/instrument-title/__tests__/instrument.title.react.js
@@ -3,6 +3,7 @@ import { InstrumentTitle } from "../instrument.title.react";
 import React from "react";
 import { mount } from "enzyme";
 import td from "testdouble";
+import DrumMachineActions from "../../../actions/root.actions";
 
 describe("InstrumentTitle", () => {
   const dispatchSpy = td.function();
@@ -33,5 +34,19 @@ describe("InstrumentTitle", () => {
       dispatch={dispatchSpy}
     ></InstrumentTitle>);
     expect(wrapper.exists(".instrument-title")).to.be.false;
+  });
+  it("changes the instrument name", () => {
+    const wrapper = mount(<InstrumentTitle
+      instruments={[
+        {
+          id: 0,
+          type: "drumMachine",
+          selected: true
+        }
+      ]}
+      dispatch={dispatchSpy}
+    ></InstrumentTitle>);
+    wrapper.find("DefaultInput").props().onValueChange("test name")
+    td.verify(DrumMachineActions.instruments.changeInstrumentName(0, "test name"));
   });
 });
