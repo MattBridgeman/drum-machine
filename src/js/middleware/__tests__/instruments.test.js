a4328dfbaac277dc7164ab9a48e61b180c30ebd7
Matthew Bridgeman <matthew.bridgeman@boats.com>
2019-05-04 19:43:57 +0100

    test work

--- src/js/middleware/__tests__/instruments.test.js
+++ src/js/middleware/__tests__/instruments.test.js
@@ -59,4 +59,66 @@ describe("Instruments", () => {
       instruments: []
     })));
   });
+
+  it("updates 'ON_NEW_INSTRUMENT' with id, machineId and instrumentType", () => {
+    let store = {
+      getState: () => ({
+        instruments: [{
+          id: 0,
+          type: "drumMachine",
+          machineId: 0
+        }],
+        drumMachine: {
+          0: {
+            banks: {
+              0: []
+            }
+          }
+        }
+      })
+    };
+    let next = td.function();
+    let nodes = [{outputs: { channels: [] }}];
+    td.when(updateInstrumentAudio(td.matchers.anything()))
+      .thenReturn(nodes);
+    let newAction = instruments(store)(next);
+    newAction({type: "ON_NEW_INSTRUMENT", instrumentType: "drumMachine"});
+    td.verify(next({
+      type: "ON_NEW_INSTRUMENT",
+      id: 1,
+      machineId: 1,
+      instrumentType: "drumMachine"
+    }));
+  });
+
+  it("updates 'ON_NEW_INSTRUMENT' with id, machineId and instrumentType", () => {
+    let store = {
+      getState: () => ({
+        instruments: [{
+          id: 0,
+          type: "synth",
+          machineId: 0
+        }],
+        synth: {
+          0: {
+            banks: {
+              0: []
+            }
+          }
+        }
+      })
+    };
+    let next = td.function();
+    let nodes = [{outputs: { channels: [] }}];
+    td.when(updateInstrumentAudio(td.matchers.anything()))
+      .thenReturn(nodes);
+    let newAction = instruments(store)(next);
+    newAction({type: "ON_NEW_INSTRUMENT", instrumentType: "synth"});
+    td.verify(next({
+      type: "ON_NEW_INSTRUMENT",
+      id: 1,
+      machineId: 1,
+      instrumentType: "synth"
+    }));
+  });
 });
