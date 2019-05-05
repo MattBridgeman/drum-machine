60d95f88982751c9e8caa88757b850509f797232
Matthew Bridgeman <matthew.bridgeman@boats.com>
2019-05-05 10:56:36 +0100

    test work

--- src/js/components/fader/__tests__/fader.react.test.js
+++ src/js/components/fader/__tests__/fader.react.test.js
@@ -5,6 +5,16 @@ import { mount } from "enzyme";
 import td from "testdouble";
 
 describe("Fader", () => {
+  let raf;
+  beforeEach(() => {
+    td.replace(window, "requestAnimationFrame", (cb) => {
+      raf = cb;
+    });
+  });
+  afterEach(() => {
+    raf = null;
+    td.reset();
+  });
 	it("renders the instruments and an add instrument", () => {
     const onValueChangeSpy = td.function();
     const wrapper = mount(<Fader
@@ -21,6 +31,41 @@ describe("Fader", () => {
       height={10}
     />);
     expect(wrapper.exists(".fader-container")).to.be.true;
+  });
+	it("renders a vertical fader", () => {
+    const onValueChangeSpy = td.function();
+    const wrapper = mount(<Fader
+      min={1}
+      max={10}
+      step={1}
+      value={1}
+      onValueChange={onValueChangeSpy}
+      name="Fader Name"
+      id="fader"
+      orientation="vertical"
+      type="test-type"
+      width={10}
+      height={200}
+    />);
+    expect(wrapper.exists(".fader-container")).to.be.true;
+  });
+	it("unmounts correctly", () => {
+    const onValueChangeSpy = td.function();
+    const wrapper = mount(<Fader
+      min={1}
+      max={10}
+      step={1}
+      value={1}
+      onValueChange={onValueChangeSpy}
+      name="Fader Name"
+      id="fader"
+      orientation="horizontal"
+      type="test-type"
+      width={200}
+      height={10}
+    />);
+    wrapper.unmount();
+    expect(wrapper.exists(".fader-container")).to.be.false;
   });
   it("drags the fader", () => {
     const onValueChangeSpy = td.function();
@@ -58,6 +103,41 @@ describe("Fader", () => {
       preventDefault: td.function(),
       target: "foo"
     });
+    td.verify(onValueChangeSpy(1.09));
+  });
+  it("drags the fader", () => {
+    const onValueChangeSpy = td.function();
+    const wrapper = mount(<Fader
+      min={1}
+      max={10}
+      step={1}
+      value={1}
+      onValueChange={onValueChangeSpy}
+      name="Fader Name"
+      id="fader"
+      orientation="horizontal"
+      type="test-type"
+      width={200}
+      height={10}
+    />);
+    const FaderInstance = wrapper.instance();
+    FaderInstance.onStart({
+      preventDefault: td.function(),
+      target: "foo",
+      touches: [{
+        pageX: 0,
+        pageY: 0
+      }]
+    });
+    FaderInstance.onMove({
+      preventDefault: td.function(),
+      target: "foo",
+      touches: [{
+        pageX: 100,
+        pageY: 0
+      }]
+    });
+    raf();
     td.verify(onValueChangeSpy(1.09));
   });
 });
